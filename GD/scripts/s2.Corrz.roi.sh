d6833484a41eacb15d048caaaf5fcfbe9608173b
psb629 <psb629@gmail.com>
2020-10-26 21:49:10 +0900

    s2.Corrz.roi.sh completed

--- GD/scripts/s2.Corrz.roi.sh
+++ GD/scripts/s2.Corrz.roi.sh
@@ -1,7 +1,6 @@
 #!/bin/tcsh
 
-#set subj_list = (GD11 GD07 GD30 GD02 GD29 GD32 GD23 GD01 GD31 GD33 GD20 GD44 GD26 GD15)
-set subj_list = (GD30 GD02 GD29 GD32 GD23 GD01 GD31 GD33 GD20 GD44 GD26 GD15)
+set subj_list = (GD11 GD07 GD30 GD02 GD29 GD32 GD23 GD01 GD31 GD33 GD20 GD44 GD26 GD15)
 
 set root_dir = /Volumes/T7SSD1/GD
 set data_dir = $root_dir/connectivity/rest_WM_Vent_BP
@@ -10,8 +9,8 @@ set output_dir = $data_dir/CorrZ.caudate
 set roi = (caudate_head_R caudate_body_R caudate_tail_R caudate_head_L caudate_body_L caudate_tail_L)
 
 foreach n (`count -digit 1 1 $#roi`)
+	@ b = $n - 1
 	set temp = ()
-	@ n = $n - 1
 	foreach subj ($subj_list)
 		set pname = $output_dir/CorrZ.caudate.$subj.rest.WM
 		if ( ! -e $pname+tlrc.HEAD ) then
@@ -22,9 +21,9 @@ foreach n (`count -digit 1 1 $#roi`)
 		endif
     	## head_R, body_R, tail_R, head_L, body_L, tail_L
 		set pname = $output_dir/CorrZ.$roi[$n].$subj.rest.WM
-		3dcalc -prefix $pname -a "$output_dir/CorrZ.caudate.$subj.rest.WM+tlrc.HEAD[$n]" -expr a
-		set temp = ($temp $pname+tlrc.HEAD)
+		3dcalc -prefix $pname -a "$output_dir/CorrZ.caudate.$subj.rest.WM+tlrc.HEAD[$b]" -expr a
+		set temp = ($temp $pname+tlrc)
 	end
-	3dbucket -prefix $output_dir/CorrZ.$roi[$n].GA.n$#subj_list.rest.WM $temp
+	3dbucket $temp -prefix $output_dir/CorrZ.$roi[$n].GA.n$#subj_list.rest.WM
 end
 
