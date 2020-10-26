7671a1b9efd2baef9a4ac22afcfab5a49cb7e6d2
psb629 <psb629@gmail.com>
2020-10-26 21:49:34 +0900

    s3.Corr.roi.sh almost completed

--- GD/scripts/s3.Corr.roi.sh
+++ GD/scripts/s3.Corr.roi.sh
@@ -10,28 +10,27 @@ set output_dir = $data_dir/Corr.caudate
 set roi = (caudate_head_R caudate_body_R caudate_tail_R caudate_head_L caudate_body_L caudate_tail_L)
 
 foreach n (`count -digit 1 1 $#roi`)
-	@ n = $n - 1
 	# =======================================================
-	3dDeconvolve -input "CorrZ.$roi.GA.All_n30.rest.WM+tlrc[0..27,29]" \
-		-mask /Volumes/clmnlab/GA/MVPA/fullmask_GAGB/full_mask_GAGB_n30+tlrc \
-		-num_stimts 1 -stim_file 1 rew_tot_n29_ex_outlier.1D -stim_label 1 rew_tot \
-		-fout -tout -polort 0 -bucket Corr.$roi.GA.rew_tot.rest.WM_n29_ex_outlier
-
-	3dDeconvolve -input "CorrZ.$roi.GA.All_n30.rest.WM+tlrc[0..27,29]" \
-		-mask /Volumes/clmnlab/GA/MVPA/fullmask_GAGB/full_mask_GAGB_n30+tlrc \
-		-num_stimts 1 -stim_file 1 lr_n29_ex_outlier.1D -stim_label 1 lr \
-		-fout -tout -polort 0 -bucket Corr.$roi.GA.lr.rest.WM_n29_ex_outlier
-	# =======================================================
-	set pname = $output_dir/Corr.$roi[$n].GD.lr.rest.WM_n${#subj_list}.new
-	3dTcorr1D -prefix $pname \
-		-float -mask $full_mask \
-		"CorrZ.$roi[$n].GA.All_n${#subj_list}.rest.WM+tlrc" lr_n${#subj_list}.1D
-	3dAFNItoNIFTI $pname+tlrc $output_dir/$roi[$n].GD.lr.rest.WM_n${#subj_list}.new
+	3dDeconvolve -input "CorrZ.$roi[$n].GD.n${#subj_list}.rest.WM+tlrc" \
+		-mask $full_mask
+		-num_stimts 1 -stim_file 1 rew_tot_n${#subj_list}.1D -stim_label 1 rew_tot \
+		-fout -tout -polort 0 -bucket Corr.$roi.GD.rew_tot.rest.WM_n$#subj_list
 
 	set pname = $output_dir/Corr.$roi[$n].GD.rew_tot.rest.WM_n${#subj_list}.new
 	3dTcorr1D -prefix $pname \
 		-float -mask $full_mask \
-		"CorrZ.$roi[$n].GA.All_n30.rest.WM+tlrc" rew_tot_n${#subj_list}.1D
-	 3dAFNItoNIFTI Corr.$roi.GA.rew_tot.rest.WM_n${#subj_list}_ex_outlier.new+tlrc $roi[$n].GA.rew_tot.rest.WM_n${#subj_list}_ex_outlier.new
+		"CorrZ.$roi[$n].GD.n$#subj_list.rest.WM+tlrc" rew_tot_n${#subj_list}.1D
+	3dAFNItoNIFTI Corr.$roi[$n].GD.rew_tot.rest.WM_n${#subj_list}.new+tlrc \
+		$roi[$n].GA.rew_tot.rest.WM_n${#subj_list}.new
+	# =======================================================
+#	3dDeconvolve -input "CorrZ.$roi.GA.All_n30.rest.WM+tlrc[0..27,29]" \
+#		-mask $full_mask
+#		-num_stimts 1 -stim_file 1 lr_n29_ex_outlier.1D -stim_label 1 lr \
+#		-fout -tout -polort 0 -bucket Corr.$roi.GA.lr.rest.WM_n29_ex_outlier
+#	set pname = $output_dir/Corr.$roi[$n].GD.lr.rest.WM_n${#subj_list}.new
+#	3dTcorr1D -prefix $pname \
+#		-float -mask $full_mask \
+#		"CorrZ.$roi[$n].GD.All_n${#subj_list}.rest.WM+tlrc" lr_n${#subj_list}.1D
+#	3dAFNItoNIFTI $pname+tlrc $output_dir/$roi[$n].GD.lr.rest.WM_n${#subj_list}.new
 end
 
