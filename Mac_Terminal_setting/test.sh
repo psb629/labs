1e8c75527ff7cf0523187a93da4fb01e1ffb6d34
psb629 <psb629@gmail.com>
2020-11-21 18:42:47 +0900

    added test.sh

--- Mac_Terminal_setting/test.sh
+++ Mac_Terminal_setting/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd ~/Github/labs
 git config --global user.name "psb629"
 git config --global user.email "psb629@gmail.com"
 git remote remove origin
 git remote add origin https://’psb629’:’na6607!!MS’@github.com/psb629/labs.git
-
+git push --set-upstream origin master
