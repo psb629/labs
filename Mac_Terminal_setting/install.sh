9d23ac49a12418ce7c1fa3f178ef08b9b84d5b68
psb629 <psb629@gmail.com>
2020-11-21 18:51:52 +0900

    install.sh almost completed

--- Mac_Terminal_setting/install.sh
+++ Mac_Terminal_setting/install.sh
@@ -18,10 +18,17 @@ cd ~/Github
 git clone https://github.com/psb629/labs.git
 #git clone https://github.com/clmnlab/labs.git
 cd ~/Github/labs
+git init
 git config --global user.name "psb629"
 git config --global user.email "psb629@gmail.com"
 git remote remove origin
-git remote add origin https://’psb629’:’na6607!!MS’@github.com/psb629/labs.git
+git remote add origin https://'psb629':'na6607!!MS'@github.com/psb629/labs.git
+git pull origin master
+
+echo `date` >>~/Github/labs/Mac_Terminal_setting/README.txt
+git add ~/Github/labs/Mac_Terminal_setting/README.txt
+git commit -m "ran install.sh"
+git push -u origin master
 
 #### vim ####
 cd ~
