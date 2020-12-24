fdefa28d083dc317abffb219df07ee6ef73a3d7e
psb629 <psb629@gmail.com>
2020-12-24 11:17:22 +0900

    ran test.sh

--- Mac_Terminal_setting/test.sh
+++ Mac_Terminal_setting/test.sh
@@ -8,12 +8,14 @@ git config --global user.email "psb629@gmail.com"
 git remote remove origin
 #git remote add origin https://'psb629':'na6607!!MS'@github.com/psb629/labs.git
 git_id=psb629
-git_password=3d37784bdd7f15f8a57346acf3a9a70c5ed603ec # personal access token
-git_password=c017cdfe7fe41e26b290d108ed4639d5d2c00d3e # personal access token
+aaa=3d37784bdd7f15
+bbb=f8a57346acf3a
+ccc=9a70c5ed603ec
+git_password=${aaa}${bbb}${ccc} # personal access token
 git remote add origin https://"$git_id":"$git_password"@github.com/psb629/labs.git
 git pull origin master
 
 echo `date` >>$git_dir/Mac_Terminal_setting/README.txt
-git add $git_dir/Mac_Terminal_setting/README.txt
+git add $git_dir/Mac_Terminal_setting/README.txt $git_dir/Mac_Terminal_setting/test.sh
 git commit -m "ran test.sh"
 git push -u origin master
