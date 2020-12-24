e7a3f04bd427fc0e5bfb21578b580edbc78f2a4f
psb629 <psb629@gmail.com>
2020-12-24 11:00:56 +0900

    created my_permission token

--- Mac_Terminal_setting/install.sh
+++ Mac_Terminal_setting/install.sh
@@ -16,23 +16,24 @@ brew install git
 mkdir ~/Github
 cd ~/Github
 git clone https://github.com/psb629/labs.git
- ##git clone https://github.com/clmnlab/labs.git
- #git_dir=~/Github/labs
- #cd $git_dir
- #git init
- #git config --global user.name "psb629"
- #git config --global user.email "psb629@gmail.com"
- #git remote remove origin
- ##git remote add origin https://'psb629':'na6607!!MS'@github.com/psb629/labs.git
- #git_id=psb629
- #git_password=3d37784bdd7f15f8a57346acf3a9a70c5ed603ec # personal access token
- #git remote add origin https://"$git_id":"$git_password"@github.com/psb629/labs.git
- #git pull origin master
- #
- #echo `date` >>$git_dir/Mac_Terminal_setting/README.txt
- #git add $git_dir/Mac_Terminal_setting/README.txt
- #git commit -m "ran install.sh"
- #git push -u origin master
+#git clone https://github.com/clmnlab/labs.git
+git_dir=~/Github/labs
+cd $git_dir
+git init
+git config --global user.name "psb629"
+git config --global user.email "psb629@gmail.com"
+git remote remove origin
+#git remote add origin https://'psb629':'na6607!!MS'@github.com/psb629/labs.git
+git_id=psb629
+git_password=3d37784bdd7f15f8a57346acf3a9a70c5ed603ec # personal access token
+git_password=c017cdfe7fe41e26b290d108ed4639d5d2c00d3e # personal access token
+git remote add origin https://"$git_id":"$git_password"@github.com/psb629/labs.git
+git pull origin master
+
+echo `date` >>$git_dir/Mac_Terminal_setting/README.txt
+git add $git_dir/Mac_Terminal_setting/README.txt
+git commit -m "ran install.sh"
+git push -u origin master
 
 #### vim ####
 cd ~
--- Mac_Terminal_setting/test.sh
+++ Mac_Terminal_setting/test.sh
@@ -9,10 +9,11 @@ git remote remove origin
 #git remote add origin https://'psb629':'na6607!!MS'@github.com/psb629/labs.git
 git_id=psb629
 git_password=3d37784bdd7f15f8a57346acf3a9a70c5ed603ec # personal access token
+git_password=c017cdfe7fe41e26b290d108ed4639d5d2c00d3e # personal access token
 git remote add origin https://"$git_id":"$git_password"@github.com/psb629/labs.git
 git pull origin master
 
 echo `date` >>$git_dir/Mac_Terminal_setting/README.txt
 git add $git_dir/Mac_Terminal_setting/README.txt
-git commit -m "ran install.sh"
+git commit -m "ran test.sh"
 git push -u origin master
