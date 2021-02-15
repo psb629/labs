fd5cfece100b3740e4053cf3c7e4a87a07a160a3
psb629 <psb629@gmail.com>
2021-02-15 17:53:37 +0900

    BA done

--- GA/scripts/m0.TT_Daemon.sh
+++ GA/scripts/m0.TT_Daemon.sh
@@ -23,15 +23,29 @@ set vv_list = (94 294 BA17)
 set vv_list = (95 295 BA18)
 set vv_list = (96 296 BA19)
 set vv_list = (97 297 BA20)
- #set vv_list = (113 313 BA37)
- #set vv_list = (114 314 BA38)
+set vv_list = (113 313 BA37)
+set vv_list = (114 314 BA38)
 # =========================================================
 set vv1 = $vv_list[1]
 set vv2 = $vv_list[2]
 
-set pname = $output_dir/TT_Daemon.brik$uu.${vv_list[3]}
-3dcalc -a "${TT_resem}[$uu]" -expr "iszero(a-$vv1)+iszero(a-$vv2)" -prefix $pname
-## count the number of non-zero voxels
+ #set pname = $output_dir/TT_Daemon.brik$uu.${vv_list[3]}
+ #3dcalc -a "${TT_resem}[$uu]" -expr "iszero(a-$vv1)+iszero(a-$vv2)" -prefix $pname
+ ### count the number of non-zero voxels
+ #set nn = `3dBrickStat -count -non-zero $pname+tlrc`
+ #3dAFNItoNIFTI -prefix $pname.n$nn.nii.gz $pname+tlrc
+ #rm $pname+tlrc.*
+# =========================================================
+## right-side
+set pname = $output_dir/TT_Daemon.brik$uu.${vv_list[3]}R
+3dcalc -a "${TT_resem}[$uu]" -expr "iszero(a-$vv1)" -prefix $pname
+set nn = `3dBrickStat -count -non-zero $pname+tlrc`
+3dAFNItoNIFTI -prefix $pname.n$nn.nii.gz $pname+tlrc
+rm $pname+tlrc.*
+# =========================================================
+## left_side
+set pname = $output_dir/TT_Daemon.brik$uu.${vv_list[3]}L
+3dcalc -a "${TT_resem}[$uu]" -expr "iszero(a-$vv2)" -prefix $pname
 set nn = `3dBrickStat -count -non-zero $pname+tlrc`
 3dAFNItoNIFTI -prefix $pname.n$nn.nii.gz $pname+tlrc
 rm $pname+tlrc.*
