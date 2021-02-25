5568b2f37c9d0b27d734b34b715aa178b068e8d1
psb629 <psb629@gmail.com>
2021-02-25 17:52:25 +0900

    print(%.4f)

--- Samsung_Hospital/scripts/affine_transform.sh
+++ Samsung_Hospital/scripts/affine_transform.sh
@@ -1,18 +1,21 @@
 #!/bin/tcsh
 
-set subj = S23
+set subj = S22
 
-set root_dir = /Users/clmnlab/Desktop
+set root_dir = /Users/clmn/Desktop/Samsung_Hospital
 set fmri_dir = $root_dir/fmri_data
 set preproc_dir = $fmri_dir/preproc_data
 set data_dir = $preproc_dir/$subj/preprocessed
 
-set affine_matrix = $data_dir/deoblique.aff.2D
+set affine_matrix = $data_dir/deoblique.$subj.aff.2D
+cat $affine_matrix
 # ===================================================
 ## A coordinate of the target
-set vec_r = (10 -22 -40)
+set target = (-49.154 -63.342 22.470)
+set hippo = (-29.333 -17.344 -3.750)
+set vec_r = (-29.333 -17.344 -3.750)
 # ===================================================
-## calculation
+## Affine transformation
 set vec_r = ($vec_r 1)
 set vec_rp = ()
 foreach ll (`count -digits 2 2 4`)
@@ -20,7 +23,7 @@ foreach ll (`count -digits 2 2 4`)
 	set sum = 0
 	foreach xx (`count -digits 2 1 4`)
 		set temp = `python -c "print($line[$xx]*$vec_r[$xx])"`
-		set sum = `python -c "print($temp+$sum)"`
+		set sum = `python -c "print('%.4f'%($temp+$sum))"`
 	end
 	set vec_rp = ($vec_rp $sum)
 end
