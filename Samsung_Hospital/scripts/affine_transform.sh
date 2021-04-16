77da9c0e30c9177d518c701eee0bc0306f3c1fb3
psb629 <psb629@gmail.com>
2021-04-16 18:04:58 +0900

    refined affine_transform.sh, not surely.

--- Samsung_Hospital/scripts/affine_transform.sh
+++ Samsung_Hospital/scripts/affine_transform.sh
@@ -1,32 +1,49 @@
 #!/bin/tcsh
 
-set subj = S22
+## @auto_tlrc:
+## The script will output the final transform in a 1D file with the
+## extension Xat.1D, say THAT_NAME.1D
+## Call this transform Mt and let Xt and Xo be the 4x1 column vectors
+## coordinates of the same voxel in standard (t) and original (o)
+## space, respectively. The transform is such that Xo = Mt Xt 
+## You can use this transform to manually warp a volume in orig
 
-set root_dir = /Users/clmn/Desktop/Samsung_Hospital
+set subj = S24
+
+set root_dir = /Volumes/T7SSD1/samsung_hospital
 set fmri_dir = $root_dir/fmri_data
 set preproc_dir = $fmri_dir/preproc_data
 set data_dir = $preproc_dir/$subj/preprocessed
 
-set affine_matrix = $data_dir/deoblique.$subj.aff.2D
+## The final transform:
+set affine_matrix = $data_dir/warp.$subj.anat.Xat.1D
+#set affine_matrix = $data_dir/deoblique.$subj.aff.2D
+echo "Mt="
 cat $affine_matrix
 # ===================================================
 ## A coordinate of the target
 set target = (-49.154 -63.342 22.470)
 set hippo = (-29.333 -17.344 -3.750)
-set vec_r = (-29.333 -17.344 -3.750)
+set Xo = (-16.599 -24.297 -8.661)
+set Xt = (-25 -36 -6)
 # ===================================================
 ## Affine transformation
-set vec_r = ($vec_r 1)
-set vec_rp = ()
-foreach ll (`count -digits 2 2 4`)
+set Xt = ($Xt 1)
+set vec_r = ()
+foreach ll (`count -digits 2 1 3`)
 	set line = `head -${ll} $affine_matrix |tail -1`
 	set sum = 0
 	foreach xx (`count -digits 2 1 4`)
-		set temp = `python -c "print($line[$xx]*$vec_r[$xx])"`
+		set temp = `python -c "print($line[$xx]*$Xt[$xx])"`
 		set sum = `python -c "print('%.4f'%($temp+$sum))"`
 	end
-	set vec_rp = ($vec_rp $sum)
+	set vec_r = ($vec_r $sum)
 end
 # ===================================================
 ## result
-echo $vec_rp
+echo "Xt="
+echo $Xt
+echo "Mt*Xt="
+echo $vec_r
+echo "Xo="
+echo $Xo
