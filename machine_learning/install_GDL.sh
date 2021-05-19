1ecf5c1d47a7391d22af6d95943e19cc78f74b62
psb629 <psb629@gmail.com>
2021-05-19 22:14:03 +0900

    ready to follow Generatvie Deep Learning

--- machine_learning/install_GDL.sh
+++ machine_learning/install_GDL.sh
@@ -1,10 +1,18 @@
+#!/bin/zsh
+
+git_dir=~/Github
+ #cd $git_dir
+ #git clone https://github.com/rickiepark/GDL_code.git
+
+GDL_dir=$git_dir/GDL_code
+ #cd $GDL_dir
+ #git pull
+
 env_name=GDL
-
 conda create -n $env_name python=3.6 ipykernel
-
 source activate $env_name
 is_env_activated=`conda info | grep $env_name | wc -l`
 if [ $is_env_activated -gt 0 ]; then
 	echo "ready to pip install modules at env '${env_name}'"
-	pip install virtualenv virtualenvwrapper
+	pip install -r $GDL_dir/requirements.txt
 fi
