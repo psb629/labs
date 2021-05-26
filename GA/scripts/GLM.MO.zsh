ce0e342e3a4996ebd956c147762713452e539bb4
psb629 <psb629@gmail.com>
2021-05-26 18:09:49 +0900

    executed GLM.MO.zsh

---
+++ GA/scripts/GLM.MO.zsh
@@ -0,0 +1,113 @@
+#!/bin/zsh
+
+nn_list=( 01 02 05 07 08 \
+		  11 12 13 14 15 \
+		  18 19 20 21 23 \
+		  26 27 28 29 30 \
+		  31 32 33 34 35 \
+		  36 37 38 42 44 )
+# ============================================================
+dname=GLM.MO
+# ============================================================
+data_dir=/Volumes/GoogleDrive/내\ 드라이브/GA/pb02
+root_dir=/Volumes/GoogleDrive/내\ 드라이브/GA
+behav_dir=$root_dir/behav_data
+fmri_dir=$root_dir/fMRI_data
+roi_dir=$fmri_dir/roi
+stats_dir=$fmri_dir/stats
+# ============================================================
+foreach nn ($nn_list)
+	## make the output directory
+	output_dir=$stats_dir/$dname/$nn
+	if [ ! -d $output_dir ]; then
+		mkdir -p -m 755 $output_dir
+	fi
+	## make a temporal directory to activate 3dDeconvolve, then re-define the output_dir
+	temp_dir=/Users/clmn/Desktop/temp
+	if [ ! -d $temp_dir ]; then
+		mkdir -p -m 755 $temp_dir
+	fi
+	output_dir=$temp_dir/$dname/$nn
+	if [ ! -d $output_dir ]; then
+		mkdir -p -m 755 $output_dir
+	fi
+	foreach gg (GA GB)
+		subj=$gg$nn
+		foreach rr (`count -digits 2 1 6`)
+			## move main files to the temp_dir directory
+			pb02=pb02.$subj.r$rr.volreg.nii.gz
+			cp -n $data_dir/$pb02 \
+				$temp_dir/$pb02
+			censor=motion_censor.$subj.r$rr.1D
+			cp -n $fmri_dir/preproc_data/$nn/$censor \
+				$temp_dir/$consor
+			mask=full_mask.$subj.nii.gz
+			cp -n $roi_dir/full/$mask \
+				$temp_dir/$mask
+			cd $output_dir
+			## AM = Amplitude Modulation
+			## The -stim_times_AM* options have been modified to allow the input of multiple amplidues with each time.
+			## -stim_times_AM1 still builds only 1 regressor, as before amplitude of each BLOCK (say) is modulated by sum of all extra amplitudes provided.
+			3dDeconvolve -input $temp_dir/$pb02 \
+						-censor $temp_dir/$censor \
+						-mask $temp_dir/$mask \
+						-local_times -polort A -float \
+						-num_stimts 7 \
+						-num_glt 1 \
+						-stim_times_AM1 1 $behav_dir/regressors/4targets/$subj.AMregressor.4targets.r$rr.1D 'BLOCK(5,1)' \
+						-stim_label 1 MO \
+						-stim_file 2 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[0]" -stim_base 2 -stim_label 2 roll \
+						-stim_file 3 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[1]" -stim_base 3 -stim_label 3 pitch \
+						-stim_file 4 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[2]" -stim_base 4 -stim_label 4 yaw \
+						-stim_file 5 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[3]" -stim_base 5 -stim_label 5 dS \
+						-stim_file 6 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[4]" -stim_base 6 -stim_label 6 dL \
+						-stim_file 7 "$fmri_dir/preproc_data/$nn/motion_demean.$subj.r$rr.1D[5]" -stim_base 7 -stim_label 7 dP \
+						-x1D $output_dir/$subj.X.MO.r$rr.1D \
+						-xjpeg $output_dir/$subj.X.MO.r$rr.jpg \
+						-x1D_uncensored $output_dir/$subj.X.nocensor.MO.r$rr.1D \
+						-errts $output_dir/$subj.errts.MO.r$rr.nii.gz \
+						-bucket $output_dir/$subj.stats.MO.r$rr.nii.gz
+
+			3dTproject -polort 0 -input $output_dir/$subj.errts.MO.r$rr.nii.gz \
+					-mask $temp_dir/$mask \
+					-passband 0.01 0.1 \
+					-cenmode ZERO \
+					-prefix $output_dir/$subj.bp_demean.errts.MO.r$rr.nii.gz
+			## move the temporal directory to Google Drive
+			cp -n $temp_dir/$dname/$nn/. $stats_dir/$dname/$nn/
+			## remove useless files
+			rm $temp_dir/$pb02 \
+				$temp_dir/$censor \
+				$temp_dir/$mask
+			rm -r $temp_dir/$dname/$nn
+
+		end
+	end
+end
+ #	# ============================================================
+ #	3dAFNItoNIFTI -prefix $output_dir/statMove.$nn.nii.gz $output_dir/statMove.$nn+tlrc.
+ #	# ============================================================
+ # #	gzip -1v $output_dir/*.BRIK
+ #	rm $output_dir/statMove.$nn+tlrc.*
+ #	# ============================================================
+ #	echo "subject GA$nn completed"
+ #end
+# ============================================================
+## group t-test
+ #set output_dir = $stats_dir/$dname
+ #set group_dir = $output_dir/group
+ #if ( ! -d $group_dir ) then
+ #	mkdir -p -m 755 $group_dir
+ #endif
+ #
+ #set temp = ()
+ #foreach nn ($subj_list)
+ #	3dcalc -a $output_dir/$nn/"statMove.$nn.nii.gz[7]" -expr "a" -prefix $group_dir/temp.$nn.nii.gz
+ #	set temp = ($temp $group_dir/temp.$nn.nii.gz)
+ #end
+ #
+ #set gmask = $roi_dir/full_mask.GAs.nii.gz
+ #set pname = $output_dir/group.statMove.nii.gz
+ #3dttest++ -mask $gmask -setA $temp -prefix $pname
+ #rm -r $group_dir
+# ============================================================
---
+++ GA/scripts/test.zsh
@@ -0,0 +1,68 @@
+#!/bin/zsh
+
+nn_list=( 01 02 05 07 08 \
+		  11 12 13 14 15 \
+		  18 19 20 21 23 \
+		  26 27 28 29 30 \
+		  31 32 33 34 35 \
+		  36 37 38 42 44 )
+# ==================================================================
+x=1
+let x+=1
+echo 1:$x
+((x+=1))
+echo 2:$x
+x=$[$x+1]
+echo 3:$x
+x=`expr $x + 1`
+echo 4:$x
+x=`echo "$x+1"|bc`
+echo 5:$x
+x=3.14
+x=`echo "scale=2;$x*2"|bc`
+echo 6:$x
+
+foreach x (1 2)
+	if [ $x -eq 1 ]; then
+		echo "$x -eq 1 : true"
+	 #elif [ ! $x -eq 1 ]; then
+	 #	echo "false"
+	 else;
+	 	echo "$x -eq 1 : else"
+	fi
+end
+
+foreach x (`count -digits 2 0 3`)
+	if [ $x -eq "0" ]; then
+		echo "$x finded!"
+	else;
+		echo "$x"
+	fi
+end
+# ==================================================================
+nn_list=( 01 )
+dname=GLM.MO
+
+data_dir=/Volumes/GoogleDrive/내\ 드라이브/GA/pb02
+root_dir=/Volumes/GoogleDrive/내\ 드라이브/GA
+behav_dir=$root_dir/behav_data
+fmri_dir=$root_dir/fMRI_data
+roi_dir=$fmri_dir/roi
+stats_dir=$fmri_dir/stats
+
+foreach nn ($nn_list)
+	foreach gg (GA)
+		subj=$gg$nn
+		foreach rr (01)
+			## AM = Amplitude Modulation
+			## The -stim_times_AM* options have been modified to allow the input of multiple amplidues with each time.
+			## -stim_times_AM1 still builds only 1 regressor, as before amplitude of each BLOCK (say) is modulated by sum of all extra amplitudes provided.
+			3dDeconvolve -nodata 1096 0.46 \
+						-polort A -float \
+						-num_stimts 1 \
+						-num_glt 1 \
+						-stim_times_AM1 1 $behav_dir/regressors/4targets/$subj.AMregressor.4targets.r$rr.1D 'BLOCK(5,1)' \
+						-x1D /Users/clmn/Github/labs/GA/scripts/AM.$subj.r$rr -x1D_stop
+		end
+	end
+end
