fdd1edc8d03d6cfdc2e475aa08644be92e6f5cbf
psb629 <psb629@gmail.com>
2021-07-07 18:16:29 +0900

    Regressing global signal out of voxel time series

--- GA/scripts/3dTproject.bp.global_activity.zsh
+++ GA/scripts/3dTproject.bp.global_activity.zsh
@@ -7,36 +7,38 @@ nn_list=( 01 02 05 07 08 \
 		  31 32 33 34 35 \
 		  36 37 38 42 44 )
 # ============================================================
+dname=GLM.MO
+# ============================================================
+data_dir=/Volumes/GoogleDrive/내\ 드라이브/GA/pb02
 root_dir=/Volumes/GoogleDrive/내\ 드라이브/GA
 behav_dir=$root_dir/behav_data
 fmri_dir=$root_dir/fMRI_data
 roi_dir=$fmri_dir/roi
 stats_dir=$fmri_dir/stats
 # ============================================================
-mask=fan.roi.GA.all.nii.gz
-# ============================================================
 work_dir=~/Desktop/temp
 if [ ! -d $work_dir ]; then
 	mkdir -p -m 755 $work_dir
 fi
-cp -n $roi_dir/fan280/$mask $work_dir
 # ============================================================
 foreach nn ($nn_list)
+	fin_dir=$stats_dir/GLM.MO/$nn
+	if [ ! -d $fin_dir ]; then
+		mkdir -p -m 755 $fin_dir
+	fi
 	foreach gg (GA GB)
 		subj=$gg$nn
-		foreach run (r01 r02 r03 r04 r05 r06)
-			data=$subj.errts.MO.$run.nii.gz
-			cp -n $stats_dir/GLM.MO/$nn/$data $work_dir
-
-			cd $work_dir
-			3dmaskave -quiet -mask $mask $data > $subj.errts.MO.$run.global_activity.1D
-			rm $data
+		foreach rr (`seq -f "%02g" 1 6`)
+			fin_res=$subj.global_activity.bp_demean.errts.MO.r$rr.nii.gz
+			3dTproject -polort 0 -input $stats_dir/GLM.MO/$nn/$subj.errts.MO.r$rr.nii.gz \
+					-ort $stats_dir/GLM.MO/tsmean/fan_all/$subj.errts.MO.$run.global_activity.1D \
+					-mask $temp_dir/$mask \
+					-passband 0.01 0.1 \
+					-cenmode ZERO \
+					-prefix $work_dir/$fin_res
+			cp $work_dir/$fin_res $fin_dir/$fin_res
+			rm $work_dir/$fin_res
 		end
 	end
 end
 # ============================================================
-fin_dir=$stats_dir/GLM.MO/tsmean/fan_all
-if [ ! -d $fin_dir ]; then
-	mkdir -p -m 755 $fin_dir
-fi
-cp -n $work_dir/*.1D $fin_dir
