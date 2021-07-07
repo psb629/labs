98b5844f1a6ba0066186ae3accf11178801a2d11
psb629 <psb629@gmail.com>
2021-07-07 18:48:14 +0900

    Complete calculation up to participant #20

--- GA/scripts/3dTproject.bp.global_activity.zsh
+++ GA/scripts/3dTproject.bp.global_activity.zsh
@@ -6,6 +6,9 @@ nn_list=( 01 02 05 07 08 \
 		  26 27 28 29 30 \
 		  31 32 33 34 35 \
 		  36 37 38 42 44 )
+nn_list=( 01 02 05 07 08 \
+		  11 12 13 14 15 \
+		  18 19 20 )
 # ============================================================
 dname=GLM.MO
 # ============================================================
@@ -28,17 +31,27 @@ foreach nn ($nn_list)
 	fi
 	foreach gg (GA GB)
 		subj=$gg$nn
+		## move the mask data to work_dir
+		mask=full_mask.$subj.nii.gz
+		cp -n $roi_dir/full/$mask $work_dir/$mask
 		foreach rr (`seq -f "%02g" 1 6`)
 			fin_res=$subj.global_activity.bp_demean.errts.MO.r$rr.nii.gz
-			3dTproject -polort 0 -input $stats_dir/GLM.MO/$nn/$subj.errts.MO.r$rr.nii.gz \
-					-ort $stats_dir/GLM.MO/tsmean/fan_all/$subj.errts.MO.$run.global_activity.1D \
-					-mask $temp_dir/$mask \
+			## move the input file to work_dir
+			input=$subj.errts.MO.r$rr.nii.gz
+			cp -n $stats_dir/GLM.MO/$nn/$input $work_dir/$input
+			## main
+			3dTproject -polort 0 -input $work_dir/$input\
+					-ort $stats_dir/GLM.MO/tsmean/fan_all/$subj.errts.MO.r$rr.global_activity.1D \
+					-mask $work_dir/$mask \
 					-passband 0.01 0.1 \
 					-cenmode ZERO \
 					-prefix $work_dir/$fin_res
-			cp $work_dir/$fin_res $fin_dir/$fin_res
-			rm $work_dir/$fin_res
+			## clear
+			cp -n $work_dir/$fin_res $fin_dir/$fin_res
+			rm $work_dir/$fin_res $work_dir/$input
 		end
+		## clear
+		rm $work_dir/$mask
 	end
 end
 # ============================================================
