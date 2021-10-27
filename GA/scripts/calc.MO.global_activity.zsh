d135bd3cbedbff7c912eab08cdcbd702a938e5c4
psb629 <psb629@gmail.com>
2021-10-27 17:21:49 +0900

    modified...

--- GA/scripts/calc.MO.global_activity.zsh
+++ GA/scripts/calc.MO.global_activity.zsh
@@ -7,55 +7,34 @@ list_nn=( 01 02 05 07 08 \
 		  31 32 33 34 35 \
 		  36 37 38 42 44 )
 # ============================================================
-dir_data=/home/sungbeenpark/GoogleDrive/GA
-dir_behav=$dir_data/behav_data
-dir_fmri=$dir_data/fMRI_data
+dir_root=/home/sungbeenpark
+dir_fmri=$dir_root/GoogleDrive/GA/fMRI_data
 dir_roi=$dir_fmri/roi
-dir_stats=$dir_fmri/stats
+dir_stats=$dir_fmri/stats/GLM.MO
 # ============================================================
-region='full_mask'
-mask=full_mask.GAs.nii.gz
+ #region='full_mask'
+ #mask=full_mask.GAs.nii.gz
+region='fan_all'
+mask=fan.roi.GA.all.nii.gz
 # ============================================================
-dir_fin=$dir_stats/GLM.MO/tsmean/$region
+dir_fin=$dir_root/GLM.MO/tsmean/$region
 if [ ! -d $dir_fin ]; then
 	mkdir -p -m 755 $dir_fin
 fi
 # ============================================================
-dir_work=~/tsmean
-if [ ! -d $dir_work ]; then
-	mkdir -p -m 755 $dir_work
-fi
-cp -n $dir_roi/$mask $dir_work
-# ============================================================
 foreach nn ($list_nn)
-	foreach gg (GA GB)
+	dir_data=$dir_stats/$nn
+	foreach gg ('GA' 'GB')
 		subj=$gg$nn
+		echo "$subj"
 		foreach run (r01 r02 r03 r04 r05 r06)
+			echo "$run..."
 			data=$subj.bp_demean.errts.MO.$run.nii.gz
 			fname=tsmean.bp_demean.errts.MO.$subj.$run.$region.1D
 			
-			## 만약 최종 output 이 없으면, 계산을 위한 데이터 복사부터 시작
-			if [ ! -e $dir_fin/$fname ]; then
-				if [ ! -e $dir_work/$data ]; then
-					echo "copying $data to $dir_work"
-					cp -n $dir_stats/GLM.MO/$nn/$data $dir_work
-				fi
-				echo "Calculating ${gg}${nn} $run $region ..."
-	 			dir_output=$dir_work/$region
-				if [ ! -d $dir_output ]; then
-					mkdir -p -m 755 $dir_output
-				fi
-				3dmaskave -quiet -mask $dir_work/$mask $dir_work/$data >$dir_output/$fname
-			fi
-			if [ -e $dir_work/$data ]; then
-				rm $dir_work/$data
-			fi
+			3dmaskave -quiet -mask $dir_roi/fan280/$mask $dir_data/$data >$dir_fin/$fname
 		end
 	end
 end
 # ============================================================
-rm $dir_work/$mask
-# ============================================================
-find $dir_work -type f -size 0 -delete
-find $dir_fin -type f -size 0 -delete
-cp -n -r $dir_work/$region/* $dir_fin
+ #find $dir_work -type f -size 0 -delete
