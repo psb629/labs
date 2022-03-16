e3c33a0b2feb24887b54e37280539f176808e08e
psb629 <psb629@gmail.com>
2022-03-16 17:10:27 +0900

    modified...

--- GA/scripts/mk.fan280.zsh
+++ GA/scripts/mk.fan280.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-fan_dir=~/Desktop/Fan280
+dir_fan=/mnt/sda2/GA/fmri_data/masks/fan280
 master=~/Desktop/Fan280/pb02.GA01.r01.volreg.nii.gz
 
 output_dir=~/Desktop/fan280
@@ -9,5 +9,5 @@ if [ ! -d $output_dir ]; then
 fi
 
 foreach nn (`seq -f '%03g' 1 280`)
-	3dresample -master $master -prefix $output_dir/fan.roi.GA.$nn.nii.gz -input $fan_dir/fan.roi.$nn.nii.gz
+	3dresample -master $master -prefix $output_dir/fan.roi.GA.$nn.nii.gz -input $dir_fan/fan.roi.$nn.nii.gz
 end
--- GA/scripts/mk.global_fan280.sh
+++ GA/scripts/mk.global_fan280.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
-fan_dir=~/Desktop/Fan280_GA
-output_dir=$fan_dir
+dir_fan=/mnt/sda2/GA/fmri_data/masks/fan280
+dir_output=$dir_fan
 
 list=()
 foreach nn (`seq -f "%03g" 1 280`)
-	list=($list $fan_dir/fan.roi.GA.$nn.nii.gz)
+	list=($list $dir_fan/fan.roi.GA.$nn.nii.gz)
 end
 
-pname=$output_dir/fan.roi.GA.all.nii.gz
+pname=$dir_output/fan.roi.GA.all.nii
 3dmerge -gmax -prefix $pname $list
 
 3dBrickStat -count -non-zero $pname
