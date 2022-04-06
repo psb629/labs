9afbeb8f582b54e7175b64ed3121e6be4dfd2013
psb629 <psb629@gmail.com>
2022-04-06 16:47:09 +0900

    calculated time series of GLM.RO in Mohr264 ROIs

---
+++ GA/scripts/calc.time_series.GLM_RO.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+from glob import glob
+import sys
+import getpass
+import os
+# import psutil
+from os.path import join, exists
+from os.path import getsize
+import pickle
+import numpy as np
+import pandas as pd
+import scipy
+
+from tqdm import tqdm
+
+import seaborn as sns
+import statsmodels.stats.multitest
+# from statsmodels.sandbox.stats.multicomp import multipletests
+# import nilearn.masking
+from nilearn import plotting as nplt
+from nilearn import image as niimg
+from nilearn.input_data import NiftiLabelsMasker
+from nilearn.input_data import NiftiSpheresMasker
+# from nilearn.regions import connected_label_regions
+from nilearn.connectome import ConnectivityMeasure
+import nilearn.decoding
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GroupKFold
+from sklearn.preprocessing import StandardScaler
+# from sklearn.svm import LinearSVC
+
+## experimental properties
+list_subj = ['01', '02', '05', '07', '08', '11', '12', '13', '14', '15'
+             ,'18', '19', '20', '21', '23', '26', '27', '28', '29', '30'
+             ,'31', '32', '33', '34', '35', '36', '37', '38', '42', '44']
+list_stage = ['early_practice', 'early_unpractice', 'late_practice', 'late_unpractice']
+
+## define directories
+ #dir_root = '/home/sungbeenpark/GA/tsmean/Mohr264'
+ #dir_fmri = '/mnt/sda2/GA/fmri_data'
+dir_root = '/Volumes/T7-SSD2/GA'
+dir_fmri = join(dir_root,'pb04.errts_tproject.RO.bp')
+dir_output = '/Volumes/GoogleDrive/내 드라이브/GA/fMRI_data/time_series'
+os.makedirs(dir_output, exist_ok=True)
+
+## load atlas
+seeds = np.loadtxt('/Volumes/T7-SSD2/Mohr264/Mohr264.txt', dtype=int)
+masker = NiftiSpheresMasker(
+        seeds=seeds
+        , mask_img=join(dir_root,'masks/full_mask.GAGB.nii.gz')
+        , radius=5
+        , allow_overlap=True
+        , memory=join('/Users/clmn/Desktop','nilearn_cache'), memory_level=1, verbose=0
+)
+
+## Calculating time-series mean activity for each ROI
+ #stat = 'pb04.errts_tproject'
+stat = 'GLM.RO'
+fin = 'time_series.%s.Mohr264.pkl'%stat
+
+ #path_data = glob(join(dir_fmri, stat, '??', '*.nii'))
+path_data = glob(join(dir_fmri,'??','pb04.errts_tproject.RO.bp.G???.r??.nii'))
+
+time_series = {}
+
+for path in tqdm(path_data):
+    tmp = path.split('/')[-1].split('.')
+    subj = tmp[-3]
+    run = tmp[-2]
+    
+    img = niimg.load_img(path)
+    time_series[(subj, run)] = masker.fit_transform(img)
+
+with open(join(dir_output, fin), 'wb') as f:
+    pickle.dump(time_series, f)
