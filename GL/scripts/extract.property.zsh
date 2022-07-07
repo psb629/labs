584f8574629bde9858ec8ac3adc0f8d12ed7df48
psb629 <psb629@gmail.com>
2022-07-07 13:43:54 +0900

    update codes to extract some values from GL data

--- GL/scripts/extract.property.zsh
+++ GL/scripts/extract.property.zsh
@@ -1,15 +1,29 @@
 #!/bin/zsh
 
-dir_root=/mnt/sdb2/GL/fmri_data/stats/GLM.reward
+dir_root=/mnt/ext6/GL/fmri_data/stats
 
-list_subj=(`ls $dir_root | grep GL`)
- #list_subj=('GL03')
+list_subj=(`ls $dir_root/GLM.reward | grep '^GL[0-9][0-9]'`)
 
 coord="tlrc"
+
+conda activate GA
 ##===========================================##
+# Move-Stop
+cd $dir_root/GLM.Move-Stop
+foreach subj ($list_subj)
+	foreach prop ("Move-Stop_GLT#0_Coef" "Move-Stop_GLT#0_Tstat")
+		3dcalc -prefix $subj.$prop.$coord.nii \
+			-a statMove.$subj+$coord.HEAD"[$prop]" -expr 'a'
+	end
+	TtoZ --t_stat_map="$subj.Move-Stop_GLT#0_Tstat.$coord.nii" --dof=296 --output_nii="$subj.Move-Stop.z_score_map.nii"
+end
+
+# Reward
+cd $dir_root/GLM.reward
 foreach subj ($list_subj)
 	foreach prop ("Rew#1_Coef" "Rew#1_Tstat")
-		3dcalc -prefix $dir_root/$subj/$subj.$prop.$coord.nii \
-			-a $dir_root/$subj/stats.$subj+$coord.HEAD"[$prop]" -expr 'a'
+		3dcalc -prefix $subj/$subj.$prop.$coord.nii \
+			-a $subj/stats.$subj+$coord.HEAD"[$prop]" -expr 'a'
 	end
+	TtoZ --t_stat_map="$subj/$subj.Rew#1_Tstat.$coord.nii" --dof=1690 --output_nii="$subj/$subj.reward.z_score_map.nii"
 end
--- GL/scripts/extract.striatum.zsh
+++ GL/scripts/extract.striatum.zsh
@@ -5,24 +5,47 @@ list_nn=(03 04 05 06 07 \
 		14 15 16 17 18 \
 		19 20 21 22 24 \
 		25 26 27 29)
-dir_root=/mnt/sdb2/GL/fmri_data/stats/GLM.reward
+dir_root=/mnt/ext6/GL/fmri_data/masks
+dir_atlas=/mnt/sda2/atlas
 ##===========================================##
 ## caudate (L): 11
 ## caudate (R): 50
 ## striatum (L): 12
 ## striatum (R): 51
 ## individual striatums
-foreach nn ($list_nn)
-	subj="GL$nn"
-	dir_output=$dir_root/$subj
+ #foreach nn ($list_nn)
+ #	subj="GL$nn"
+ #	## FreeSurfer
+ #	dir_output=$dir_root/FreeSurfer
+ #	if [ ! -d $dir_output ]; then
+ #		mkdir -p -m 755 $dir_output
+ #	fi
+ #	data=$dir_root/FreeSurfer/$subj.aparc+aseg.nii
+ #	3dcalc -a $data -expr 'or(equals(a,11),equals(a,50))'\
+ #		-prefix $dir_output/$subj.caudate.1mm.orig.nii
+ #	3dcalc -a $data -expr 'or(equals(a,12),equals(a,51))'\
+ #		-prefix $dir_output/$subj.putamen.1mm.orig.nii
+ #	3dcalc -a $data -expr 'or(equals(a,26),equals(a,58))'\
+ #		-prefix $dir_output/$subj.NAc.1mm.orig.nii
+ #	3dcalc -a $data -expr 'or(equals(a,11),equals(a,50),equals(a,12),equals(a,51),equals(a,26),equals(a,58))'\
+ #		-prefix $dir_output/$subj.striatum.1mm.orig.nii
+ #end
+ #
+ ### Harvard-Oxford
+ #3dcalc -a $dir_atlas/HarvardOxford/HarvardOxford-sub-maxprob-thr0-1mm.nii.gz -expr 'or(equals(a,6),equals(a,17))' \
+ #	-prefix $dir_root/HarvardOxford-sub-maxprob-thr0-1mm.putamen.nii
+ #
+ #3dcalc -a $dir_atlas/HarvardOxford/HarvardOxford-sub-maxprob-thr0-1mm.nii.gz -expr 'or(equals(a,5),equals(a,16))' \
+ #	-prefix $dir_root/HarvardOxford-sub-maxprob-thr0-1mm.caudate.nii
+ #
+ #3dcalc -a $dir_atlas/HarvardOxford/HarvardOxford-sub-maxprob-thr0-1mm.nii.gz -expr 'or(equals(a,11),equals(a,21))' \
+ #	-prefix $dir_root/HarvardOxford-sub-maxprob-thr0-1mm.NAc.nii
 
-	data=$dir_root/$subj/$subj.aparc+aseg.nii
-	3dcalc -a $data -expr 'or(equals(a,11),equals(a,12),equals(a,50),equals(a,51))'\
-		-prefix $dir_output/$subj.striatum.1mm.orig.nii
-	3dcalc -a $data -expr 'or(equals(a,11),equals(a,50))'\
-		-prefix $dir_output/$subj.caudate.1mm.orig.nii
-end
- ### MNI striatum
- #mni_mask=
- #3dcalc -a $mni -expr 'or(equals(a,),equals(a,),equals(a,),equals(a,))'\
- #	-prefix $dir_root/
+## TT_Daemon
+### Left caudate Head 326
+### Left caudate Body 325
+### Right caudate Head 126
+### Right caudate Body 125
+3dresample -prefix $dir_root/resam.TTatlas.nii -input $dir_atlas/afni/TTatlas.nii -master $dir_root/full_mask.GL+tlrc.nii
+3dcalc -a $dir_root/resam.TTatlas.nii'[uu5[0]]' -expr 'equals(a,326) + equals(a,325)*2 + equals(a,126)*3 + equals(a,125)*4' \
+	-prefix $dir_root/mask.caudate.nii
