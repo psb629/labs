ba138730422e21ba40a2a9904101f4aa306c0a0f
psb629 <psb629@gmail.com>
2022-08-04 17:22:01 +0900

    renewed 'dof' value

--- GL/scripts/extract.property.zsh
+++ GL/scripts/extract.property.zsh
@@ -2,7 +2,8 @@
 
 dir_root=/mnt/ext6/GL/fmri_data/stats
 
-list_subj=(`ls $dir_root/GLM.reward | grep '^GL[0-9][0-9]'`)
+stat='4s_shifted'
+list_subj=(`ls $dir_root/GLM.reward.$stat | grep '^GL[0-9][0-9]'`)
 
 coord="tlrc"
 
@@ -15,15 +16,19 @@ foreach subj ($list_subj)
 		3dcalc -prefix $subj.$prop.$coord.nii \
 			-a statMove.$subj+$coord.HEAD"[$prop]" -expr 'a'
 	end
-	TtoZ --t_stat_map="$subj.Move-Stop_GLT#0_Tstat.$coord.nii" --dof=296 --output_nii="$subj.Move-Stop.z_score_map.nii"
+	tstat="$subj.Move-Stop_GLT#0_Tstat.$coord.nii"
+	dof=`3dinfo -verb $tstat | grep -o -E 'statpar = [0-9]+' | grep -o -E '[0-9]+'`
+	TtoZ --t_stat_map=$tstat --dof=$dof --output_nii="$subj.Move-Stop.z_score_map.nii"
 end
 
 # Reward
-cd $dir_root/GLM.reward
+cd $dir_root/GLM.reward.$stat
 foreach subj ($list_subj)
 	foreach prop ("Rew#1_Coef" "Rew#1_Tstat")
 		3dcalc -prefix $subj/$subj.$prop.$coord.nii \
 			-a $subj/stats.$subj+$coord.HEAD"[$prop]" -expr 'a'
 	end
-	TtoZ --t_stat_map="$subj/$subj.Rew#1_Tstat.$coord.nii" --dof=1690 --output_nii="$subj/$subj.reward.z_score_map.nii"
+	tstat="$subj/$subj.Rew#1_Tstat.$coord.nii"
+	dof=`3dinfo -verb $tstat | grep -o -E 'statpar = [0-9]+' | grep -o -E '[0-9]+'`
+	TtoZ --t_stat_map=$tstat --dof=$dof --output_nii="$subj/$subj.reward.z_score_map.nii"
 end
