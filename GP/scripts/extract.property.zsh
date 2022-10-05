3af19b2487092f98cdb7fcd51b9a821401d5ca1a
psb629 <psb629@gmail.com>
2022-10-05 13:13:29 +0900

    extract betas and Tstat from 5s_shifted reward-modulated GLM results

--- GP/scripts/extract.property.zsh
+++ GP/scripts/extract.property.zsh
@@ -1,14 +1,22 @@
 #!/bin/zsh
 
-dir_root=/mnt/ext6/GP/fmri_data/stats/AM/reward
-
-list_subj=(`ls $dir_root | grep GP`)
- #list_subj=('GL03')
-coord="tlrc"
+ #dir_root=/mnt/ext6/GP/fmri_data/stats/AM/reward
+ #
+ #list_subj=(`ls $dir_root | grep GP`)
+ # #list_subj=('GL03')
+ #coord="tlrc"
+ #foreach subj ($list_subj)
+ #	foreach prop ("rwdtm#2_Coef" "rwdtm#2_Tstat")
+ #		3dcalc -prefix $dir_root/$subj/$subj.$prop.$coord.nii \
+ #			-a $dir_root/$subj/statsRWDtime.$subj.SPMG2+$coord.HEAD"[$prop]" -expr 'a'
+ #	end
+ #end
 ##===========================================##
-foreach subj ($list_subj)
-	foreach prop ("rwdtm#2_Coef" "rwdtm#2_Tstat")
-		3dcalc -prefix $dir_root/$subj/$subj.$prop.$coord.nii \
-			-a $dir_root/$subj/statsRWDtime.$subj.SPMG2+$coord.HEAD"[$prop]" -expr 'a'
-	end
-end
+dir_root=/mnt/ext6/GP/fmri_data/stats/GLM.reward.5s_shifted
+list_subj=(`find $dir_root -type d -name "GP??" | sed "s;$dir_root/;;g"`)
+
+# 'Rew#1_Coef'
+for subj in $list_subj
+	for prop in 'Rew#1_Coef' 'Rew#1_Tstat'
+		3dcalc -prefix $dir_root/$subj/$subj.$prop.nii \
+			-a $dir_root/$subj/stats.$subj+tlrc.HEAD"[$prop]" -expr 'a'
