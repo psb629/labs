f0a78841b7eeedc30401f35bbd700762b6f812f9
psb629 <psb629@gmail.com>
2022-10-12 12:58:48 +0900

    A code for copying files in parallel

--- GP/scripts/cp.parallel.zsh
+++ GP/scripts/cp.parallel.zsh
@@ -1,27 +1,18 @@
 #!/bin/zsh
 
-dir_current=`pwd`
-
-dir_from=/mnt/sdb2/GP/fmri_data/preproc_data
-dir_to=/mnt/ext6/GP/fmri_data/preproc_data
-
-## make directories in the objective directory
-find $dir_from -type d >$dir_current/dir_from.txt
-sed "s.$dir_from.$dir_to.g" $dir_current/dir_from.txt >$dir_current/dir_to.txt
-foreach dir (`cat $dir_current/dir_to.txt`)
-	if [ ! -d $dir ]; then
-		mkdir -p -m 755 $dir
-	fi
-end
-
-## check files for copy
-find $dir_from -type f >$dir_current/dir_from.txt
-sed "s;$dir_from;;g" $dir_current/dir_from.txt >$dir_current/fname.txt
-
-rm $dir_current/dir_from.txt $dir_current/dir_to.txt
-
- #cat $dir_current/fname.txt | xargs -n3500 echo >$dir_current/tmp.txt
-
+#=============================================
+list_nn=( 08 09 10 11 17 18 19 20 21 22 \
+		  24 26 27 32 33 34 35 36 37 38 \
+		  39 40 41 42 43 44 45 46 47 48 \
+		  49 50 51 53 54 55 )
+#=============================================
+dir_from="/mnt/ext6/GP_KJH/fmri_data/raw_data"
+dir_to="/mnt/ext6/GP/fmri_data/raw_data"
+#=============================================
+##copy directories
+find "$dir_from" -type d | sed "s;$dir_from;$dir_to;g" | xargs mkdir -p -m 755
+##copy files
+find "$dir_from" -type f | sed "s;$dir_from;;g" >list_fname.txt
 ## If the number of arguments > 4000, error would be occured.
-parallel -j10 cp -n $dir_from{} $dir_to{} ::: `cat $dir_current/fname.txt`
-
+cat ./list_fname.txt | xargs -n1 | parallel -j8 cp -r -n "$dir_from{}" "$dir_to{}"
+ #parallel -j8 cp -r -n "$dir_from{}" "$dir_to{}" ::: `cat list_fname.txt`
