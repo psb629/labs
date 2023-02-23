139f2a2b785f51b7845050a6611397750dde20f6
psb629 <psb629@gmail.com>
2023-02-24 02:09:50 +0900

    Did get a result of 3dttest++ for GLM.reward_per_trial

--- GA/scripts/3dttest++.reward_per_trial.shifted.zsh
+++ GA/scripts/3dttest++.reward_per_trial.shifted.zsh
@@ -32,7 +32,7 @@ dir_output=$dir_stat
 ## ============================================================ ##
 list_subj=(`ls $dir_stat | grep "G.[0-9][0-9]"`)
 ## ============================================================ ##
-mask="$dir_mask/mask.group.day2.n42.frac=0.7.nii"
+mask="$dir_mask/mask.group.GA.n30.frac=0.7.nii"
 ## ============================================================ ##
 cd $dir_output
 
@@ -41,10 +41,10 @@ for subj in $list_subj
 {
 	setA=($setA $dir_stat/$subj/stats.Rew.$subj.nii'[Rew#1_Coef]')
 }
-3dttest++ -mask $mask					\
-	-setA $setA							\
-	-prefix GA.toz.n$#list_dlpfc.nii	\
+3dttest++ -mask $mask							\
+	-setA $setA									\
+	-prefix group.Clustsim.n$#list_subj.nii	\
 	-ClustSim 4
 ## ============================================================ ##
 ## extract the Z stat
-3dcalc -a GA.toz.n$#list_subj.nii'[SetA_Zscr]' -expr 'a' -prefix GA.Zscr.n$#list_subj.nii
+3dcalc -a group.Clustsim.n$#list_subj.nii'[SetA_Zscr]' -expr 'a' -prefix group.Zscr.n$#list_subj.nii
