27c3e88bc3efc7d73aace85e6214f0b3e989bab6
psb629 <psb629@gmail.com>
2023-02-28 11:23:57 +0900

    added an option '-p'

--- DRN/scripts/resize_images.py
+++ DRN/scripts/resize_images.py
@@ -7,20 +7,34 @@ from os.path import join, exists
 from glob import glob
 import sys
 import shutil
-
+import re
+# ======================================================== #
 if ('-s' in sys.argv):
     idx = sys.argv.index('-s')
 elif ('--subj' in sys.argv):
     idx = sys.argv.index('--subj')
 subj=str(sys.argv[idx+1])
-
+# ======================================================== #
+if ('-p' in sys.argv):
+    idx = sys.argv.index('-p')
+    pxl=str(sys.argv[idx+1])
+elif ('--pixel' in sys.argv):
+    idx = sys.argv.index('--pixel')
+    pxl=str(sys.argv[idx+1])
+elif ('--pixels' in sys.argv):
+    idx = sys.argv.index('--pixels')
+    pxl=str(sys.argv[idx+1])
+else:
+    pxl = '(128,72)'
+w,h = np.array(re.findall('\d+', pxl)).astype(int)
+# ======================================================== #
 dir_root = join("/mnt/ext5/DRN/behav_data", subj)
 if not exists(dir_root):
     raise Exception(" %s doesn't exist!"%dir_root)
-
+# ======================================================== #
 list_dir_run = sorted(glob(join(dir_root, "Run?")))
 dir_resize = join(dir_root, 'resized')
-
+# ======================================================== #
 for dir_run in list_dir_run:
     run = dir_run.split('/')[-1]
     dir_output = join(dir_resize, run)
@@ -35,5 +49,5 @@ for dir_run in list_dir_run:
         img = Image.open(fname)
 #         print(img.size)
         step = int(fname.split('/')[-1].split('.')[0])
-        img_resized = img.resize((128,72))
+        img_resized = img.resize((w,h))
         img_resized.save(join(dir_output, '%05d.png'%step))
