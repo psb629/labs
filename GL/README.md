9e8cb8a693e268fab5e8f64eb1d9447bd2eaf8b8
psb629 <psb629@gmail.com>
2023-03-22 15:15:23 +0900

    Completion of 3dExtrema code

--- GL/README.md
+++ GL/README.md
@@ -20,7 +20,7 @@
 - [GLM.reward_per_trial.shifted.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/GLM.reward_per_trial.shifted.zsh) -s subject -t time_shift (default=0s)
 
 ### extracting a BOLD signal from a voxel which has maximux beta value
-- [3dExtrema.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/3dExtrema.zsh) -s subject
+- [3dExtrema.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/3dExtrema.zsh) -s subject -a area -R ROI
 
 ### group analysis (2nd level analysis)
 #### GLM
--- GL/scripts/3dExtrema.zsh
+++ GL/scripts/3dExtrema.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 ##############################################################
+area=false
+vv=false
+##############################################################
 ## $# = the number of arguments
 while (( $# )); do
 	key="$1"
@@ -11,9 +14,63 @@ while (( $# )); do
 		-s | --subject)
 			subj="$2"
 		;;
+		-a | --area)
+			area="$2"
+		;;
+		-R | --ROI)
+			ROI="$2"
+		;;
 	esac
 	shift ##takes one argument
 done
+##############################################################
+check=false
+case $ROI in
+	'caudate' | 'Caudate')
+		check=true
+		case $area in
+			'la')
+				vv=1
+			;;
+			'lp')
+				vv=2
+			;;
+			'ra')
+				vv=3
+			;;
+			'rp')
+				vv=4
+			;;
+		esac
+	;;
+	'putamen' | 'Putamen')
+		check=true
+		case $area in
+			'la')
+				vv=1
+			;;
+			'lp')
+				vv=3
+			;;
+			'ra')
+				vv=2
+			;;
+			'rp')
+				vv=4
+			;;
+		esac
+		;;
+esac
+if [ $check = false ]; then
+	print " You need to put a right value for an option -R."
+	print " e.g.) -R caudate, -R putamen"
+	exit
+fi
+if [ $vv = false ]; then
+	print " You need to put a right value for an option -a."
+	print " e.g.) -a la (left anterior), -a rp (right posterior)"
+	exit
+fi
 ##############################################################
 dir_root="/mnt/ext4/GL/fmri_data"
 dir_mask="$dir_root/masks"
@@ -21,16 +78,32 @@ dir_preproc="$dir_root/preproc_data.SSKim/$subj"
 dir_stat="$dir_root/stats/GLM.reward.4s_shifted.SSKim/$subj"
 dir_output=$dir_stat
 ##############################################################
-pname=$dir_output/"mask.3dExtrema.caudate.$subj.nii"
-3dExtrema														\
-	-volume -interior -quiet									\
-	-maxima														\
-	-mask_file $dir_mask/"mask.TTatlas.caudate.resampled.nii"	\
-	-prefix $pname												\
-	$dir_stat/stats.$subj+tlrc.HEAD'[Rew#0_Coef]'
+## make temparal masks
+mask=$dir_output/"mask.tmp.$ROI.$area.nii"
+3dcalc												\
+	-a $dir_mask/"mask.TTatlas.$ROI.resampled.nii"	\
+	-expr "equals(a,$vv)"							\
+	-prefix $mask									\
 
-3dmaskave													\
-	-quiet													\
-	-mask $pname											\
-	$dir_preproc/"tproject.errts.$subj.volreg.r02_05.nii"	\
-	>$dir_output/"ts.caudate.$subj.1D"
+fname=$dir_stat/stats.$subj+tlrc.HEAD'[Rew#1_Tstat]'
+pname=$dir_output/"mask.3dExtrema.$ROI.$area.$subj.nii"
+3dExtrema						\
+	-volume -closure			\
+	-mask_file $mask			\
+	-mask_thr 1					\
+	-maxima						\
+	-nbest 1					\
+	-data_thr 0					\
+	-sep_dist 512				\
+	-prefix $pname				\
+	$fname
+rm $mask
+mask=$pname
+##############################################################
+## extract the entire BOLD signal across all runs from the voxel
+fname=$dir_preproc/"tproject.errts.$subj.volreg.r02_05.nii"
+3dmaskave											\
+	-quiet											\
+	-mask $mask										\
+	$fname >$dir_output/"ts.$ROI.$area.$subj.1D"
+rm $mask
