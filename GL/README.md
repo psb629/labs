439efcbb32f1afdea82d9ad39210673ba6ff3fa7
psb629 <psb629@gmail.com>
2023-04-13 18:05:37 +0900

    Re-doing the GLM, and the results from GLM.reward_per_trial.4s_shifted.KJH seem to be almost identical.

--- GL/README.md
+++ GL/README.md
@@ -19,7 +19,7 @@
 
 ### 1st level analysis
 #### GLM
-- [GLM.reward_per_trial.shifted.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/GLM.reward_per_trial.shifted.zsh) -s subject -t time_shift (default=0s)
+- [GLM.reward_per_trial.shifted.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/GLM.reward_per_trial.shifted.zsh) -s subject -t time_shift (default=0s) -c condition (default='On')
 
 ### extracting a BOLD signal from a voxel which has maximux beta value
 - [3dExtrema.zsh](https://github.com/psb629/labs/blob/master/GL/scripts/3dExtrema.zsh) -s subject -a area -R ROI
--- GL/scripts/GLM.reward_per_trial.shifted.zsh
+++ GL/scripts/GLM.reward_per_trial.shifted.zsh
@@ -2,78 +2,94 @@
 
 ## ============================================================ ##
 ## default
-time_shift=0
+tt=0
+cc='On'
 ## ============================================================ ##
-## $# = the number of arguments
 while (( $# )); do
 	key="$1"
 	case $key in
-##		pattern)
-##			sentence
-##		;;
 		-s | --subject)
-			## string
 			subj="$2"
 		;;
 		-t | --time_shift)
-			## string
-			time_shift="$2"
+			tt="$2"
+		;;
+		-c | --condition)
+			cc="$2"
 		;;
 	esac
 	shift ##takes one argument
 done
-tmp=`printf "%.1f\n" $time_shift`
-time_shift=$tmp
 ## ============================================================ ##
+time_shift=`printf "%.1f\n" $tt`
+## ============================================================ ##
+dir_behav="/home/sungbeenpark/Github/labs/GL/behav_data"
+dir_reg="$dir_behav/regressors/AM"
+
 dir_root="/mnt/ext4/GL"
-stat="${time_shift}s_shifted"
-
-dir_behav="$dir_root/behav_data"
-dir_reg="$dir_behav/regressors/AM/$stat"
-
 dir_fmri="$dir_root/fmri_data"
-dir_preproc="$dir_fmri/preproc_data/$subj/day2"
+dir_preproc="$dir_fmri/preproc_data.SSKim/$subj"
 ## ============================================================ ##
-dir_output="$dir_fmri/stats/AM/GLM.reward_per_trial/$stat/$subj"
+case $cc in
+	'on' | 'On')
+		cond='On'
+	;;
+	'off' | 'Off')
+		cond='Off'
+	;;
+	'test' | 'Test')
+		cond='Test'
+	;;
+esac
+case $cond in
+	'Test')
+		task='test'
+		runs=(`seq -f "r%02g" 6 1 7`)
+		reg="$dir_reg/$subj.reward.$task.shift=${time_shift}.txt"
+	;;
+	*)
+		task='main'
+		runs=(`seq -f "r%02g" 2 1 5`)
+		reg="$dir_reg/$subj.reward.${task}_${cond}.shift=${time_shift}.txt"
+	;;
+esac
+## ============================================================ ##
+MD="$dir_preproc/motion.demean.$task.1D"
+if [ -f $MD ]; then
+	rm $MD
+fi
+MC="$dir_preproc/motion.censor.$task.1D"
+if [ -f $MC ]; then
+	rm $MC
+fi
+input=()
+for run in $runs
+{
+	cat "$dir_preproc/motion_demean.$subj.$run.1D" >> $MD
+
+	cat "$dir_preproc/motion_$subj.${run}_censor.1D" >> $MC
+
+	input=($input "$dir_preproc/pb04.$subj.$run.scale+tlrc.HEAD")
+}
+## ============================================================ ##
+dir_output="$dir_fmri/stats/AM/GLM.reward_per_trial/${time_shift}s_shifted/$subj/$cond"
 if [ ! -d $dir_output ]; then
 	mkdir -p -m 755 $dir_output
 fi
 ## ============================================================ ##
-## convert motion_demean (1096,6) to motion_demean.0_margin (1096*3,6)
- #~/Github/labs/GP/scripts/concatenate.motion_demean.0_margin.py -s $subj --dir_preproc $dir_preproc
-
 cd $dir_output
-## 'SPMG2' : generate 4 regeressors ( mean f(x), mean f'(x), delta f(x), delta f'(x) )
-3dDeconvolve																			\
-	-input "$dir_preproc/pb06.$subj.r0?.scale+tlrc.HEAD"								\
-	-censor "$dir_preproc/motion_${subj}_censor.1D"										\
-	-mask "$dir_preproc/full_mask.$subj+tlrc.HEAD"										\
-    -ortvec $dir_preproc/motion_demean.1D 'motion_demean'								\
-	-polort A -float																	\
-	-allzero_OK																			\
-	-num_stimts 1																		\
-	-stim_times_AM2 1 $dir_reg/$subj.reward.txt 'BLOCK(1,1)' -stim_label 1 'Rew'		\
-	-num_glt 1																			\
-	-gltsym 'SYM: Rew' -glt_label 1 'Rew'												\
-	-jobs 1 -fout -tout																	\
-	-x1D "X.xmat.$subj.1D" -xjpeg "X.$subj.jpg"											\
-	-bucket "stats.Rew.$subj.nii"
-
- #3dDeconvolve																			\
- #	-input "$dir_preproc/pb04.$subj.r0?.scale+tlrc.HEAD"								\
- #	-censor "$dir_preproc/motion_${subj}_censor.1D"										\
- #	-mask "$dir_preproc/full_mask.$subj+tlrc.HEAD"										\
- #   	-ortvec $dir_preproc/motion_demean.$subj.r01.0_margin.1D 'motion_demean_r01'	\
- #    -ortvec $dir_preproc/motion_demean.$subj.r02.0_margin.1D 'motion_demean_r02'		\
- #    -ortvec $dir_preproc/motion_demean.$subj.r03.0_margin.1D 'motion_demean_r03'		\
- #	-polort A -float																	\
- #	-allzero_OK																			\
- #	-num_stimts 1																		\
- #	-stim_times_AM2 1 $dir_reg/$subj.reward.txt 'BLOCK(1,1)' -stim_label 1 'Rew'		\
- #	-num_glt 1																			\
- #	-gltsym 'SYM: Rew' -glt_label 1 'Rew'												\
- #	-jobs 1 -fout -tout																	\
- #	-x1D "X.xmat.$subj.1D" -xjpeg "X.$subj.jpg"											\
- #	-bucket "stats.Rew.$subj"
+3dDeconvolve														\
+	-input		$input												\
+	-mask		"$dir_preproc/full_mask.$subj+tlrc.HEAD"			\
+	-censor		$MC													\
+    -ortvec		$MD 'motion_demean'									\
+	-polort A -float												\
+	-allzero_OK														\
+	-num_stimts 1													\
+	-stim_times_AM2 1 $reg 'BLOCK(1,1)' -stim_label 1 'Rew'			\
+	-jobs 1 -fout -tout												\
+	-x1D "X.xmat.$subj.$cond.1D" -xjpeg "X.$subj.$cond.jpg"			\
+	-x1D_uncensored "X.xmat.uncensored.$subj.$cond.1D"				\
+	-bucket "stats.Rew.$subj.$cond.nii"
 
 echo " Calculating GLM for subject $subj completed"
