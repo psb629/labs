0e09183dc9c258208b7b6a074852632967949c85
psb629 <psb629@gmail.com>
2023-05-01 12:41:35 +0900

    small volume correction for Precuneus

--- SMC/README.md
+++ SMC/README.md
@@ -13,8 +13,12 @@
 
 [s3.run.afni_proc.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/s3.run.afni_proc.zsh) -p phase -F from_FreeSurfer (default=yes)
 
+---
+
 ### Check the validation
 [check.absence.py](https://github.com/psb629/labs/blob/master/SMC/scripts/check.absence.py) -p phase
+
+---
 
 ### Whole Brain Correlation
 
@@ -24,16 +28,23 @@
 
 - Make an ROI mask
 
-[a2.3dUndump.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a2.3dUndump.zsh) -r radius (default=3) -f -fraction (default=0.7) -R ROI
+[a2.3dUndump.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a2.3dUndump.zsh) -r radius (default=3) -R ROI
 
 - Calculate Pearson's correlation
 
-[a3.3dTcorr1D.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a3.3dTcorr1D.zsh) -R ROI -s subject -o phase -r radius (default=3) -G RemoveGlobalSignal (default=false) -f fraction (default=0.7)
+[a3.3dTcorr1D.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a3.3dTcorr1D.zsh) -R ROI -s subject -o phase -r radius (default=3) -G RemoveGlobalSignal (default=false)
 
 - Post-Pre changes were calculated for 12 participants in the stim group and 8 participants in the sham group using 3dttest++
 
-([print.group.py](https://github.com/psb629/labs/blob/master/SMC/scripts/print.group.py) is necessary!) [a4.3dttest++.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a4.3dttest++.zsh) -R ROI -G RemoveGlobalSignal (default=false) -f fraction (default=0.7)
+([print.group.py](https://github.com/psb629/labs/blob/master/SMC/scripts/print.group.py) is necessary!) [a4.3dttest++.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a4.3dttest++.zsh) -R ROI -G RemoveGlobalSignal (default=false) -m mask (default='full')
 
 - Calculation of correlation between behavioral changes of 20 subjects and functional MRI changes across the whole brain
 
-[a5.3dTcorr1D.wholebrain.r.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a5.3dTcorr1D.wholebrain.r.zsh) -R ROI -G RemoveGlobalSignal (default=false) -f fraction (default=0.7)
+[a5.3dTcorr1D.wholebrain.r.zsh](https://github.com/psb629/labs/blob/master/SMC/scripts/a5.3dTcorr1D.wholebrain.r.zsh) -R ROI -G RemoveGlobalSignal (default=false)
+
+---
+
+### Precuneus
+
+Firstly, compute the correlation between the whole brain map and lHP for each subject (`a3.3dTcorr1D.zsh`), and perform 3dttest++ (`a4.3dttest++.zsh`) to create a precuneus cluster mask corresponding to p=0.05.
+
--- SMC/scripts/a4.3dttest++.zsh
+++ SMC/scripts/a4.3dttest++.zsh
@@ -3,8 +3,8 @@
 ##############################################################
 ## default
 ROI=false
-RGS=false
 frac=0.7
+mm='full'
 ##############################################################
 ## $# = the number of arguments
 while (( $# )); do
@@ -21,21 +21,37 @@ while (( $# )); do
 		;;
 		-G | --RemoveGlobalSignal)
 			G_bool=$2
-			if [[ ($2 == 'y') || ($2 == 'yes') ]]; then
-				RGS=true
-			elif [[ ($2 == 'n') || ($2 == 'no') ]]; then
-				RGS=false
-			fi
 		;;
+		-m | --mask)
+			mm="$2"
+
 	esac
 	shift ##takes one argument
 done
 ##############################################################
-dir_script="/home/sungbeenpark/Github/labs/Samsung_Hospital/scripts"
+dir_script="/home/sungbeenpark/Github/labs/SMC/scripts"
 
 dir_root="/mnt/ext5/SMC/fmri_data"
 dir_stat="$dir_root/stats/correlations/$ROI"
 dir_mask="$dir_root/masks"
+##############################################################
+case $G_bool in
+	'y' | 'yes')
+		RGS=true
+	;;
+	*)
+		RGS=false
+	;;
+esac
+##############################################################
+case $mm in
+	'full' | 'Full')
+		mask="$dir_mask/mask.group.n56.frac=$frac.nii"
+	;;
+	'precuneus' | 'Precuneus')
+		mask="$dir_mask/mask.Harvard-Oxford.precuneus.nii"
+	;;
+esac
 ##############################################################
 ## intersection(post & pre)
 list_subj=(`comm -1 -2 <(ls $dir_stat/3dmaskave.$ROI.S??.pre.GlobalSignalRemoved=$RGS.t298.1D | grep -P -o "S[0-9][0-9]") <(ls $dir_stat/3dmaskave.$ROI.S??.post.GlobalSignalRemoved=$RGS.t298.1D | grep -P -o "S[0-9][0-9]")`)
@@ -54,11 +70,10 @@ for subj in $list_subj
 			-prefix $pname
 	fi
 }
-
 ## divide it by group
-tmp=(`$dir_script/print.group.py -R $ROI -G $G_bool -g 'sham'`)
+tmp=(`$dir_script/print.group.py --dir_fmri $dir_root -R $ROI -G $RGS -g 'sham'`)
 list_sham=(`comm -12 <(printf '%s\n' "${list_subj[@]}") <(printf '%s\n' "${tmp[@]}")`)
-tmp=(`$dir_script/print.group.py -R $ROI -G $G_bool -g 'stim'`)
+tmp=(`$dir_script/print.group.py --dir_fmri $dir_root -R $ROI -G $RGS -g 'stim'`)
 list_stim=(`comm -12 <(printf '%s\n' "${list_subj[@]}") <(printf '%s\n' "${tmp[@]}")`)
  #printf 'sham: %s\n' "$list_sham"
  #printf 'stim: %s\n' "$list_stim"
@@ -78,10 +93,10 @@ for subj in $list_sham
 	setB+=("$dir_stat/3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$RGS.nii")
 }
 ##############################################################
-## T test
+## T-test
 cd $dir_stat
-3dttest++ -mask "$dir_mask/mask.group.n56.frac=$frac.nii"								\
-	-setA $setA																			\
-	-setB $setB																			\
-	-prefix "3dttest++.3dTcorr1D.group.post-pre.stim_n${#list_stim}-sham_n${#list_sham}.$ROI.GlobalSignalRemoved=$RGS.nii"		\
+3dttest++ -mask $mask	\
+	-setA $setA			\
+	-setB $setB			\
+	-prefix "3dttest++.3dTcorr1D.group.post-pre.stim_n${#list_stim}-sham_n${#list_sham}.$ROI.mask=$mm.GlobalSignalRemoved=$RGS.nii"		\
 	-ClustSim 4
