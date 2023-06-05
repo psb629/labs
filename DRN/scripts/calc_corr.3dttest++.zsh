7d0952b7b37b01ee517f6144756fc2f5f3eb0b3f
psb629 <psb629@gmail.com>
2023-06-05 15:37:19 +0900

    calculated 3dttest++ for calc_corr.zsh

---
+++ DRN/scripts/calc_corr.3dttest++.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+## ===================================== ##
+## $# = the number of arguments
+while (( $# )); do
+	key="$1"
+	case $key in
+##		pattern)
+##			sentence
+##		;;
+		-s | --subject)
+			subj="$2"
+		;;
+		-l | --layer)
+			layer="$2"
+		;;
+		-h | --help)
+			echo "-s, --subject\n\t-s DRN04"
+			echo "-l, --layer\n\t-l conv1"
+			exit
+		;;
+	esac
+	shift ##takes one argument
+done
+## ===================================== ##
+dir_work=/mnt/ext5/DRN/fmri_data/encoding_model/$subj
+## ===================================== ##
+cd $dir_work
+3dttest++		\
+	-setA $dir_work/corr3D.r0?.$layer.nii	\
+	-prefix $dir_work/corr3D.3dttest++.$layer.nii
