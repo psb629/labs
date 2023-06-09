1d192b9a8858db21901a8abd38854d052e433a39
psb629 <psb629@gmail.com>
2023-06-09 13:49:36 +0900

    3dttest++ for GLM.value_function

---
+++ DRN/scripts/3dttest++.value_function.zsh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+## ============================================================ ##
+ #while (( $# )); do
+ #	key="$1"
+ #	case $key in
+ #		-t | --time_shift)
+ #			tt="$2"
+ #		;;
+ #	esac
+ #	shift ##takes one argument
+ #done
+## ============================================================ ##
+dir_root="/mnt/ext5/DRN"
+dir_fmri="$dir_root/fmri_data"
+dir_stat="$dir_fmri/stats/GLM/AM/value_function"
+## ============================================================ ##
+mask=`find $dir_fmri/masks -type f -name "mask.group.n*.frac=0.7.nii"`
+## ============================================================ ##
+cd $dir_stat
+
+setA=()
+list_fname=(`find DRN?? -type f -name "stats.Val.DRN??.nii"`)
+for fname in $list_fname
+{
+	setA=($setA "${fname}[Val#1_Coef]")
+}
+## ============================================================ ##
+cd $dir_stat
+3dttest++		\
+	-setA $setA	\
+	-mask $mask	\
+	-prefix "3dttest++.Val#1_Coef.n$#list_fname.nii"
