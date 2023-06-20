bd53e8f90b2f970f5cf3712ed1c7b4cf82a0ee74
psb629 <psb629@gmail.com>
2023-06-20 16:18:03 +0900

    removed Drone directory

--- Drone/scripts/install.mlagents.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=11.3 -c pytorch -c conda-forge
-pip install mlagents==0.26.0
-pip install protobuf==3.19.4
