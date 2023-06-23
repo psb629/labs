1be592d33ff2a4de2bd4aaff7c7da0797b5b303a
psb629 <psb629@gmail.com>
2023-06-23 17:48:53 +0900

    fixed an error

--- remove.pb0xs.zsh
+++ remove.pb0xs.zsh
@@ -3,11 +3,12 @@
 ## =============================================== ##
 dir_work=`pwd`
 ## =============================================== ##
-## =============================================== ##
 list=(`find $dir_work -type f -name "pb??.*" ! -name "*scale*" ! -name "*volreg*" ! -name "*blip*" -print`)
-if [ `echo $list | wc -l` -gt 1 ]; then
-	find $dir_work -type f -name "pb??.*" ! -name "*scale*" ! -name "*volreg*" ! -name "*blip*" -print
+if [ $#list -gt 1 ]; then
+	for fname in $list
+		print $fname
 else
+	echo " Clean!"
 	exit
 fi
 ## =============================================== ##
@@ -15,7 +16,7 @@ echo "Do you want to remove the list? (yes/no)"
 read input
 ## =============================================== ##
 case $input in
-	'y' | 'yes' | 'Y' | 'Yes' | 'YES')
+	'yes')
 		find $dir_work -type f -name "pb??.*" ! -name "*scale*" ! -name "*volreg*" ! -name "*blip*" -delete
 	;;
 	*)
