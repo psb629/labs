6ec7eb1fefd9401847704c6dd291208fdf7d5af9
psb629 <psb629@gmail.com>
2023-06-26 18:53:00 +0900

    A time shift was applied to the value regressor.

--- DRN/scripts/3dttest++.value_function.zsh
+++ DRN/scripts/3dttest++.value_function.zsh
@@ -1,19 +1,24 @@
 #!/bin/zsh
 
 ## ============================================================ ##
- #while (( $# )); do
- #	key="$1"
- #	case $key in
- #		-t | --time_shift)
- #			tt="$2"
- #		;;
- #	esac
- #	shift ##takes one argument
- #done
+## default
+tt=0
+## ============================================================ ##
+while (( $# )); do
+	key="$1"
+	case $key in
+		-t | --time_shift)
+			tt="$2"
+		;;
+	esac
+	shift ##takes one argument
+done
+## ============================================================ ##
+time_shift=`printf "%1.1f" $tt`
 ## ============================================================ ##
 dir_root="/mnt/ext5/DRN"
 dir_fmri="$dir_root/fmri_data"
-dir_stat="$dir_fmri/stats/GLM/AM/value_function"
+dir_stat="$dir_fmri/stats/GLM/AM/value_function/shift=${time_shift}s"
 ## ============================================================ ##
 mask=`find $dir_fmri/masks -type f -name "mask.group.n*.frac=0.7.nii"`
 ## ============================================================ ##
