676bcac6d5f57e31c8c99069fbb01d836d2f43fa
psb629 <psb629@gmail.com>
2023-06-27 19:27:09 +0900

    The option '-Fisher' was added to the 3dTcorr1D program.

--- SMC/scripts/a4.3dttest++.zsh
+++ SMC/scripts/a4.3dttest++.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-##############################################################
+## ======================================= ##
 ## default
 ROI=false
 frac=0.7
 mm='full'
-##############################################################
+## ======================================= ##
 ## $# = the number of arguments
 while (( $# )); do
 	key="$1"
@@ -28,22 +28,22 @@ while (( $# )); do
 	esac
 	shift ##takes one argument
 done
-##############################################################
+## ======================================= ##
 dir_script="/home/sungbeenpark/Github/labs/SMC/scripts"
 
 dir_root="/mnt/ext5/SMC/fmri_data"
 dir_stat="$dir_root/stats/correlations/$ROI"
 dir_mask="$dir_root/masks"
-##############################################################
+## ======================================= ##
 case $G_bool in
 	'y' | 'yes')
-		RGS=true
+		GSR=true
 	;;
 	*)
-		RGS=false
+		GSR=false
 	;;
 esac
-##############################################################
+## ======================================= ##
 case $mm in
 	'full' | 'Full')
 		mask="$dir_mask/mask.group.n56.frac=$frac.nii"
@@ -53,51 +53,54 @@ case $mm in
 		mask="$dir_mask/mask.Precuneus_z_3.29.nii"
 	;;
 esac
-##############################################################
+## ======================================= ##
 ## intersection(post & pre)
-list_subj=(`comm -1 -2 <(ls $dir_stat/3dmaskave.$ROI.S??.pre.GlobalSignalRemoved=$RGS.t298.1D | grep -P -o "S[0-9][0-9]") <(ls $dir_stat/3dmaskave.$ROI.S??.post.GlobalSignalRemoved=$RGS.t298.1D | grep -P -o "S[0-9][0-9]")`)
- #print $list_subj
- #
+list_pre=(`ls $dir_stat/3dmaskave.$ROI.S??.pre.GlobalSignalRemoved=$GSR.t298.1D | grep -oP "S[0-9][0-9]" | sort`)
+list_post=(`ls $dir_stat/3dmaskave.$ROI.S??.post.GlobalSignalRemoved=$GSR.t298.1D | grep -oP "S[0-9][0-9]" | sort`)
+list_subj=(`comm -12 <(printf '%s\n' $list_pre[@]) <(printf '%s\n' $list_post[@])`)
+print "list_subj(n$#list_subj): $list_subj"
+
 ## post - pre
 cd $dir_stat
 for subj in $list_subj
 {
-	pname="3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$RGS.nii"
+	pname="3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$GSR.Fisher.nii"
 	if [ ! -f $pname ]; then
 		3dcalc \
-			-a 3dTcorr1D.$ROI.$subj.post.GlobalSignalRemoved=$RGS.nii	\
-			-b 3dTcorr1D.$ROI.$subj.pre.GlobalSignalRemoved=$RGS.nii	\
+			-a 3dTcorr1D.$ROI.$subj.post.GlobalSignalRemoved=$GSR.Fisher.nii	\
+			-b 3dTcorr1D.$ROI.$subj.pre.GlobalSignalRemoved=$GSR.Fisher.nii		\
 			-expr 'a-b'													\
 			-prefix $pname
 	fi
 }
+## ======================================= ##
 ## divide it by group
-tmp=(`$dir_script/print.group.py --dir_fmri $dir_root -R $ROI -G $RGS -g 'sham'`)
-list_sham=(`comm -12 <(printf '%s\n' "${list_subj[@]}") <(printf '%s\n' "${tmp[@]}")`)
-tmp=(`$dir_script/print.group.py --dir_fmri $dir_root -R $ROI -G $RGS -g 'stim'`)
-list_stim=(`comm -12 <(printf '%s\n' "${list_subj[@]}") <(printf '%s\n' "${tmp[@]}")`)
- #printf 'sham: %s\n' "$list_sham"
- #printf 'stim: %s\n' "$list_stim"
-##############################################################
-##############################################################
+tmp=(`$dir_script/print.group.py --dir_fmri $dir_root --ROI $ROI --GSR $GSR --group 'sham'`)
+list_sham=(`comm -12 <(printf '%s\n' $list_subj[@]) <(printf '%s\n' $tmp[@])`)
+tmp=(`$dir_script/print.group.py --dir_fmri $dir_root --ROI $ROI --GSR $GSR --group 'stim'`)
+list_stim=(`comm -12 <(printf '%s\n' $list_subj[@]) <(printf '%s\n' $tmp[@])`)
+printf 'sham: %s\n' "$list_sham"
+printf 'stim: %s\n' "$list_stim"
+## ======================================= ##
 ## stim
 setA=()
 for subj in $list_stim
 {
-	setA+=("$dir_stat/3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$RGS.nii")
+	setA+=("$dir_stat/3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$GSR.Fisher.nii")
 }
-
 ## sham
 setB=()
 for subj in $list_sham
 {
-	setB+=("$dir_stat/3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$RGS.nii")
+	setB+=("$dir_stat/3dTcorr1D.$ROI.$subj.post-pre.GlobalSignalRemoved=$GSR.Fisher.nii")
 }
-##############################################################
+## ======================================= ##
 ## T-test
+pname="3dttest++.3dTcorr1D.group.post-pre.stim_n${#list_stim}-sham_n${#list_sham}.$ROI.mask=$mm.GlobalSignalRemoved=$GSR.Fisher.nii"
 cd $dir_stat
-3dttest++ -mask $mask	\
-	-setA $setA			\
-	-setB $setB			\
-	-prefix "3dttest++.3dTcorr1D.group.post-pre.stim_n${#list_stim}-sham_n${#list_sham}.$ROI.mask=$mm.GlobalSignalRemoved=$RGS.nii"		\
+3dttest++	\
+	-mask $mask	\
+	-setA $setA	\
+	-setB $setB	\
+	-prefix $pname	\
 	-ClustSim 4
--- SMC/scripts/print.group.py
+++ SMC/scripts/print.group.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+## ========================================================= ##
 import numpy as np
 import pandas as pd
 import sys
@@ -8,49 +9,46 @@ from os.path import join, exists
 from glob import glob
 import re
 from subprocess import getoutput
+import argparse
+## ========================================================= ##
+## ArgumentParser 객체 생성
+parser = argparse.ArgumentParser()
 
-## default
- #RGS = 'false'
+## 옵션 목록
+parser.add_argument('-R','--ROI', help="ROI name")
+parser.add_argument('-g','--group', help="Group name (stim or sham)")
+parser.add_argument('-G','--GSR', default='false', help="Global Signal Removed (default: false)")
+parser.add_argument('--dir_fmri', default="/mnt/ext5/SMC/fmri_data", help="The location of $dir_fmri (default: /mnt/ext5/SMC/fmri_data)")
+## ========================================================= ##
+## 명령줄 인자 파싱
+args = parser.parse_args()
 
-## parameters
-if ('-R' in sys.argv):
-    idx = sys.argv.index('-R')
-    ROI = sys.argv[idx+1]
-if ('-g' in sys.argv):
-    idx = sys.argv.index('-g')
-    group = sys.argv[idx+1]
-if ('-G' in sys.argv):
-    idx = sys.argv.index('-G')
-    tmp = str(sys.argv[idx+1])
-    RGS = tmp
-if ('--dir_fmri' in sys.argv):
-    idx = sys.argv.index('--dir_fmri')
-    tmp = str(sys.argv[idx+1])
-    dir_fmri = tmp
-
+ROI = args.ROI
+group = args.group.lower()
+GSR = args.GSR
+dir_fmri = args.dir_fmri
+## ========================================================= ##
 dir_csv = '/home/sungbeenpark/Github/labs/SMC'
-
 dir_stat = join(dir_fmri, 'stats/correlations/%s'%ROI)
-
 df = pd.read_csv(join(dir_csv, 'SMC_IDs.csv'), sep=',', index_col=None)
-
+## ========================================================= ##
 ## load total subjects
-list_pre = sorted(glob(join(dir_stat, '3dTcorr1D.%s.S??.pre.GlobalSignalRemoved=%s.nii'%(ROI, RGS))))
-list_post = sorted(glob(join(dir_stat, '3dTcorr1D.%s.S??.post.GlobalSignalRemoved=%s.nii'%(ROI, RGS))))
-
+list_pre = sorted(glob(join(dir_stat,'3dTcorr1D.%s.S??.pre.GlobalSignalRemoved=%s.Fisher.nii'%(ROI,GSR))))
+list_post = sorted(glob(join(dir_stat,'3dTcorr1D.%s.S??.post.GlobalSignalRemoved=%s.Fisher.nii'%(ROI,GSR))))
+## ========================================================= ##
 tmp1 = []
 for fname in list_pre:
-    subj = fname.split('/')[-1].split('.')[-4]
+    subj = fname.split('/')[-1].split('.')[2]
     tmp1.append(subj)
 tmp1 = sorted(set(tmp1))
 tmp2 = []
 for fname in list_post:
-    subj = fname.split('/')[-1].split('.')[-4]
+    subj = fname.split('/')[-1].split('.')[2]
     tmp2.append(subj)
 tmp2 = sorted(set(tmp2))
 
 list_subj = np.intersect1d(tmp1, tmp2)
-
+## ========================================================= ##
 ## set groups separately
 list_stim=[]
 list_sham=[]
@@ -60,7 +58,7 @@ for subj in list_subj:
         list_stim.append(subj)
     else:
         list_sham.append(subj)
-
+## ========================================================= ##
 ## print subjects from the group
 if group == 'stim':
     list_subj = list_stim
@@ -69,7 +67,7 @@ elif group == 'sham':
 
 for subj in list_subj:
     print(subj, end=' ')
-
+## ========================================================= ##
  ### return
  #if group == 'stim':
  #    res = stim
