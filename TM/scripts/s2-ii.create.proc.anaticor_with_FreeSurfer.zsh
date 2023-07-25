ba3d67b9c3903065442945cfdd2d5199b779835a
psb629 <psb629@gmail.com>
2023-07-25 10:23:40 +0900

    Run 1-3 and Run 4-5 were preprocessed separately with FreeSurfer.

--- TM/scripts/s2-ii.create.proc.anaticor_with_FreeSurfer.zsh
+++ TM/scripts/s2-ii.create.proc.anaticor_with_FreeSurfer.zsh
@@ -47,60 +47,62 @@ if [[ ! -d $dir_FreeSurfer/$subj ]]; then
 		-prefix "$dir_FreeSurfer/$subj/SUMA/brain_qw.nii"
 fi
 ## ======================================= ##
- #dir_output="$dir_root/preproc_data/$subj"
- #
- #dir_script="/home/sungbeenpark/Github/labs/DRN/scripts/afni_proc.py"
- #if [ ! -d $dir_script ]; then
- #	mkdir -p -m 755 $dir_script
- #fi
- #
- #dsets=(`find $dir_raw -type f -name "func.r??.$subj.nii" | sort -t ' ' -k 1`)
- #
- #cd $dir_script
- #afni_proc.py\
- #	-subj_id					$subj	\
- #	-out_dir					$dir_output	\
- #	-blocks						despike tshift align tlrc volreg blur mask scale regress	\
- #	-radial_correlate_blocks	tcat volreg	\
- #	-copy_anat					"$dir_FreeSurfer/$subj/SUMA/brain.nii.gz"		\
- #	-anat_has_skull				'no'	\
- #	-anat_follower				anat_w_skull anat "$dir_FreeSurfer/$subj/SUMA/T1.nii.gz"	\
- #	-anat_follower_ROI			aaseg anat	\
- #								"$dir_FreeSurfer/$subj/SUMA/aparc.a2009s+aseg_REN_all.nii.gz"	\
- #	-anat_follower_ROI			aeseg epi	\
- #								"$dir_FreeSurfer/$subj/SUMA/aparc.a2009s+aseg_REN_all.nii.gz"	\
- #	-anat_follower_ROI			FSvent epi "$dir_FreeSurfer/$subj/SUMA/fs_ap_latvent.nii.gz"	\
- #	-anat_follower_ROI			FSWe epi "$dir_FreeSurfer/$subj/SUMA/fs_ap_wm.nii.gz"		\
- #	-anat_follower_erode		FSvent FSWe	\
- #	-dsets						$dsets	\
- #	-tcat_remove_first_trs		0	\
- #	-blip_forward_dset			$dir_raw/func.r01.$subj.nii'[12]'	\
- #	-blip_reverse_dset			$dir_raw/dist_PA.$subj.nii			\
- #	-align_unifize_epi			'yes'	\
- #	-align_opts_aea				\
- #	-cost						lpc+ZZ	\
- #	-giant_move					\
- #	-resample					'off'	\
- #	-check_flip					\
- #	-tlrc_base					MNI152_2009_template_SSW.nii.gz		\
- #	-tlrc_NL_warp				\
- #	-volreg_align_to			MIN_OUTLIER	\
- #	-volreg_align_e2a			\
- #	-volreg_tlrc_warp			\
- #	-blur_size					4	\
- #	-mask_epi_anat				'yes'	\
- #	-regress_motion_per_run		\
- #	-regress_ROI_PC				FSvent 3	\
- #	-regress_ROI_PC_per_run		FSvent		\
- #	-regress_make_corr_vols		aeseg FSvent\
- #	-regress_anaticor_fast		\
- #	-regress_anaticor_label		FSWe\
- #	-regress_censor_motion		0.4	\
- #	-regress_censor_outliers	0.05\
- #	-regress_apply_mot_types	demean deriv
- # #	-regress_est_blur_epits		\
- # #	-regress_est_blur_errts		\
- # #	-html_review_style			pythonic
- # #	-tlrc_NL_warped_dsets		"$dir_FreeSurfer/$subj/SUMA/T1qw.nii" 						\
- # #								"$dir_FreeSurfer/$subj/SUMA/T1qw_Allin.aff12.1D"			\
- # #								"$dir_FreeSurfer/$subj/SUMA/T1qw_WARP.nii"					\
+dir_script="/home/sungbeenpark/Github/labs/TM/scripts/afni_proc.py"
+if [ ! -d $dir_script ]; then
+	mkdir -p -m 755 $dir_script
+fi
+
+for phase in 'Dis' 'HT'
+{
+	dir_output="$dir_root/preproc_data/$subj/$phase"
+	if [ $phase = 'Dis' ]; then
+		dsets=(`seq -f "$dir_raw/func.r%02g.$subj.nii" 1 1 3`)
+	elif [ $phase = 'HT' ]; then
+		dsets=(`seq -f "$dir_raw/func.r%02g.$subj.nii" 4 1 5`)
+	fi
+
+	cd $dir_script
+	afni_proc.py\
+		-subj_id					$subj.$phase	\
+		-out_dir					$dir_output	\
+		-blocks						despike tshift align tlrc volreg blur mask scale regress	\
+		-radial_correlate_blocks	tcat volreg	\
+		-copy_anat					"$dir_FreeSurfer/$subj/SUMA/brain.nii.gz"		\
+		-anat_has_skull				'no'	\
+		-anat_follower				anat_w_skull anat "$dir_FreeSurfer/$subj/SUMA/T1.nii.gz"	\
+		-anat_follower_ROI			aaseg anat	\
+									"$dir_FreeSurfer/$subj/SUMA/aparc.a2009s+aseg_REN_all.nii.gz"	\
+		-anat_follower_ROI			aeseg epi	\
+									"$dir_FreeSurfer/$subj/SUMA/aparc.a2009s+aseg_REN_all.nii.gz"	\
+		-anat_follower_ROI			FSvent epi "$dir_FreeSurfer/$subj/SUMA/fs_ap_latvent.nii.gz"	\
+		-anat_follower_ROI			FSWe epi "$dir_FreeSurfer/$subj/SUMA/fs_ap_wm.nii.gz"		\
+		-anat_follower_erode		FSvent FSWe	\
+		-dsets						$dsets	\
+		-tcat_remove_first_trs		0	\
+		-align_unifize_epi			'yes'	\
+		-align_opts_aea				\
+		-cost						lpc+ZZ	\
+		-giant_move					\
+		-resample					'off'	\
+		-check_flip					\
+		-tlrc_base					MNI152_2009_template_SSW.nii.gz		\
+		-tlrc_NL_warp				\
+		-volreg_align_to			MIN_OUTLIER	\
+		-volreg_align_e2a			\
+		-volreg_tlrc_warp			\
+		-blur_size					4	\
+		-mask_epi_anat				'yes'	\
+		-regress_motion_per_run		\
+		-regress_ROI_PC				FSvent 3	\
+		-regress_ROI_PC_per_run		FSvent		\
+		-regress_make_corr_vols		aeseg FSvent\
+		-regress_anaticor_fast		\
+		-regress_anaticor_label		FSWe\
+		-regress_censor_motion		0.4	\
+		-regress_censor_outliers	0.05\
+		-regress_apply_mot_types	demean deriv	\
+		-html_review_style			pythonic
+	 #	-tlrc_NL_warped_dsets		"$dir_FreeSurfer/$subj/SUMA/T1qw.nii" 						\
+	 #								"$dir_FreeSurfer/$subj/SUMA/T1qw_Allin.aff12.1D"			\
+	 #								"$dir_FreeSurfer/$subj/SUMA/T1qw_WARP.nii"					\
+}
