6f8bc76358330b692a272acc0e5200ecf4db3302
psb629 <psb629@gmail.com>
2024-01-15 13:44:44 +0900

    The MNI coordinates for subjects S18, S21, and S22 seem to be abnormal.

--- SMC/scripts/affine_transformator.py
+++ SMC/scripts/affine_transformator.py
@@ -17,6 +17,7 @@ parser.add_argument('-s','--subject', help="Subject ID")
 parser.add_argument('-o','--order', default='lpi', help="RAI vs. LPI (default)")
 parser.add_argument('-m','--master', default='orig', help="Orig (default) vs. MNI")
 parser.add_argument('--xyz', help="coordinate, e.g.) '-15.241 18.516 9.663'")
+parser.add_argument('-i','--invert', help="Inverted Affine transformation")
 ## ========================================================= ##
 ## 명령줄 인자 파싱
 args = parser.parse_args()
@@ -31,14 +32,20 @@ xyz = np.concatenate([xyz,[1]]).reshape(4,1)
 ## ========================================================= ##
 dir_root = '/mnt/ext5/SMC/fmri_data'
 
-dir_work = join(dir_root,'preproc_data/To_search.target',subj)
+ #dir_work = join(dir_root,'preproc_data/To_search.target',subj)
+dir_work = join(dir_root,'preproc_data/pre.anaticor/with_FreeSurfer',subj)
 ## ========================================================= ##
 ## Affine Trasformation Matrix (MNI to Orig)
-aff1D = np.loadtxt(
-            join(dir_work,'warp.%s.anat.Xat.1D'%subj),
+ #aff2D = np.loadtxt(
+ #            join(dir_work,'warp.%s.anat.Xat.1D'%subj),
+ #            dtype='float', delimiter=None
+ #        ).reshape(3,4)
+aff2D = np.loadtxt(
+            join(dir_work,'anat.un.aff.Xat.1D'),
             dtype='float', delimiter=None
         )
-aff2D = np.vstack([aff1D.reshape(3,4), [0, 0, 0, 1]])
+aff2D = np.vstack([aff2D, [0, 0, 0, 1]])
+print(aff2D)
 aff2D_inverted = np.linalg.inv(aff2D)
 if master=='orig':
     G = aff2D_inverted
@@ -57,4 +64,5 @@ G = np.matmul(G,M)
 ## ========================================================= ##
 res = np.matmul(G,xyz)
 
+print(order)
 print(res[:3])
